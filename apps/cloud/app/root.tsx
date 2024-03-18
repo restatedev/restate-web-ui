727fa6bed96a787e4abfa6030f9265f48ad59dae
Nik Nasr <nik@restate.dev>
2024-03-18 18:18:28 +0000

    add tailwind styles

--- apps/cloud/app/root.tsx
+++ apps/cloud/app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css?url";
+import type { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- apps/cloud/app/routes/_index.tsx
+++ apps/cloud/app/routes/_index.tsx
@@ -1,16 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+    { title: 'New Remix SPA' },
+    { name: 'description', content: 'Welcome to Remix (SPA Mode)!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{' '}
       <ul>
         <li>
           <a
