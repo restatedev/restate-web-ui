b596f1fc04f4a33d61e1fddc6470c28ede166bfb
Nik <nikrooz.n@gmail.com>
2024-03-18 19:00:37 +0000

    Setup a skeleton monorepo (#1)

---
+++ apps/cloud/app/routes/_index.tsx
@@ -0,0 +1,34 @@
+import type { MetaFunction } from '@remix-run/node';
+import { Button } from '@restate/libs/ui';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'New Remix SPA' },
+    { name: 'description', content: 'Welcome to Remix (SPA Mode)!' },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1 className="text-3xl font-bold underline">Hello restate!</h1>
+      <Button />
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/future/spa-mode"
+            rel="noreferrer"
+          >
+            SPA Mode Guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ apps/cloud/vite.config.ts
@@ -0,0 +1,36 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/cloud',
+  plugins: [
+    !process.env.VITEST && remix({
+      ssr: false,
+    }),
+    nxViteTsPaths(),
+  ],
+
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    setupFiles: ['test-setup.ts'],
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/cloud',
+      provider: 'v8',
+    },
+  },
+});
