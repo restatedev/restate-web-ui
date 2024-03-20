b57cc6e9b781c741a1a67e3929f4271c85d0ab81
Nik Nasr <nik@restate.dev>
2024-03-20 22:18:32 +0000

    remove service worker js file

--- libs/util/mock-api/src/lib/enableMockApi.ts
+++ libs/util/mock-api/src/lib/enableMockApi.ts
@@ -1,6 +1,6 @@
 import { setupWorker } from 'msw/browser';
-import Worker from './mockServiceWorker/mockServiceWorker.js?url';
-import { cloudApiHandlers } from './cloudApiHandlers';
+import Worker from 'msw/mockServiceWorker.js?url';
+import { cloudApiHandlers } from './fixtures/cloudApiHandlers';
 
 export async function enableMockApi() {
   return await setupWorker(...cloudApiHandlers).start({
