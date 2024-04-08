230f32ada5f49907bd5c4b528c610c9a91386fa2
Nik Nasr <nik@restate.dev>
2024-04-08 16:55:13 +0100

    update spec

--- libs/data-access/cloud-api-client/src/lib/api/index.d.ts
+++ libs/data-access/cloud-api-client/src/lib/api/index.d.ts
@@ -20,6 +20,14 @@ export interface paths {
     /** Creates an environment */
     post: operations['createEnvironment'];
   };
+  '/cloud/{accountId}/DescribeEnvironment': {
+    /** Returns the environments details */
+    post: operations['describeEnvironment'];
+  };
+  '/cloud/{accountId}/DestroyEnvironment': {
+    /** Destroys an environment */
+    post: operations['destroyEnvironment'];
+  };
   '/cloud/{accountId}/ListEnvironments': {
     /** Returns all the environments for an account */
     post: operations['listEnvironments'];
@@ -27,6 +35,18 @@ export interface paths {
   '/cloud/{accountId}/CreateApiKey': {
     /** Creates an api key on an environment with the specified role */
     post: operations['createApiKey'];
+  };
+  '/cloud/{accountId}/DescribeApiKey': {
+    /** Returns the api key details */
+    post: operations['describeApiKey'];
+  };
+  '/cloud/{accountId}/DeleteApiKey': {
+    /** Deletes the api key */
+    post: operations['deleteApiKey'];
+  };
+  '/cloud/{accountId}/ListApiKeys': {
+    /** Returns all the api keys */
+    post: operations['listApiKeys'];
   };
 }
 
@@ -42,26 +62,69 @@ export interface components {
     Account: {
       /** @description The account id */
       accountId: string;
+      /** @description The account description */
+      description?: string;
     };
-    Environment: {
+    EnvironmentId: {
       /** @description The environment id */
       environmentId: string;
     };
+    Environment: components['schemas']['EnvironmentId'] & {
+      /** @description The environment description */
+      description?: string;
+      /**
+       * @description The environment status
+       * @enum {string}
+       */
+      status?:
+        | 'PROVISIONING_NEW'
+        | 'PROVISIONING_CREATING'
+        | 'PROVISIONING_READY'
+        | 'PROVISIONING_SHUTTING_DOWN'
+        | 'PROVISIONING_DELETED'
+        | 'PROVISIONING_FAILED'
+        | 'UNRECOGNIZED';
+    };
     CreateAccountResponse: components['schemas']['Account'];
+    CreateAccountRequestBody: {
+      /** @description The account description */
+      description?: string;
+    };
     ListAccountsResponse: {
       accounts: components['schemas']['Account'][];
     };
-    CreateEnvironmentResponse: components['schemas']['Environment'];
+    CreateEnvironmentRequestBody: {
+      /** @description The account description */
+      description?: string;
+    };
+    CreateEnvironmentResponse: components['schemas']['EnvironmentId'];
+    DestroyEnvironmentRequestBody: components['schemas']['EnvironmentId'];
+    DestroyEnvironmentResponse: {
+      error?: string;
+    };
+    DescribeEnvironmentRequestBody: components['schemas']['EnvironmentId'];
+    DescribeEnvironmentResponse: components['schemas']['Environment'];
     ListEnvironmentsResponse: {
-      environments: components['schemas']['Environment'][];
+      environments: components['schemas']['EnvironmentId'][];
     };
     /** @enum {string} */
-    Role: 'rst:role::FullAccess';
+    Role:
+      | 'rst:role::FullAccess'
+      | 'rst:role::IngressAccess'
+      | 'rst:role::AdminAccess'
+      | 'rst:role::ResolveAwakeableAccess';
     /** @enum {string} */
-    ApiKeyState: 'KEY_ACTIVE';
+    ApiKeyState:
+      | 'KEY_NEW'
+      | 'KEY_ACTIVE'
+      | 'KEY_DISABLED'
+      | 'KEY_DELETED'
+      | 'UNRECOGNIZED';
     CreateApiKeyRequestBody: {
       roleId: components['schemas']['Role'];
       environmentId: string;
+      /** @description The environment description */
+      description?: string;
     };
     CreateApiKeyResponse: {
       keyId?: string;
@@ -71,6 +134,27 @@ export interface components {
       accountId: string;
       apiKey: string;
     };
+    DescribeApiKeyRequestBody: {
+      keyId: string;
+    };
+    DescribeApiKeyResponse: components['schemas']['CreateApiKeyResponse'] & {
+      description?: string;
+    };
+    DeleteApiKeyRequestBody: {
+      keyId: string;
+      environmentId: string;
+    };
+    DeleteApiKeyResponse: {
+      error?: string;
+    };
+    ListApiKeysRequestBody: {
+      keyId: string;
+      environmentId: string;
+    };
+    ListApiKeysResponse: {
+      keyId?: string;
+      environmentId?: string;
+    }[];
   };
   responses: {
     /** @description Access token is missing or invalid */
@@ -113,6 +197,12 @@ export interface operations {
   };
   /** Creates a cloud account */
   createAccount: {
+    /** @description Specify the description */
+    requestBody?: {
+      content: {
+        'application/json': components['schemas']['CreateAccountRequestBody'];
+      };
+    };
     responses: {
       /** @description OK */
       200: {
@@ -144,11 +234,65 @@ export interface operations {
         accountId: components['parameters']['AccountId'];
       };
     };
+    /** @description Specify the description */
+    requestBody?: {
+      content: {
+        'application/json': components['schemas']['CreateEnvironmentRequestBody'];
+      };
+    };
     responses: {
       /** @description OK */
       200: {
         content: {
           'application/json': components['schemas']['CreateEnvironmentResponse'];
+        };
+      };
+      401: components['responses']['UnauthorizedError'];
+      500: components['responses']['InternalServerError'];
+    };
+  };
+  /** Returns the environments details */
+  describeEnvironment: {
+    parameters: {
+      path: {
+        accountId: components['parameters']['AccountId'];
+      };
+    };
+    /** @description Specify the environment id */
+    requestBody?: {
+      content: {
+        'application/json': components['schemas']['DescribeEnvironmentRequestBody'];
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          'application/json': components['schemas']['DescribeEnvironmentResponse'];
+        };
+      };
+      401: components['responses']['UnauthorizedError'];
+      500: components['responses']['InternalServerError'];
+    };
+  };
+  /** Destroys an environment */
+  destroyEnvironment: {
+    parameters: {
+      path: {
+        accountId: components['parameters']['AccountId'];
+      };
+    };
+    /** @description Specify the environment id */
+    requestBody?: {
+      content: {
+        'application/json': components['schemas']['DestroyEnvironmentRequestBody'];
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          'application/json': components['schemas']['DestroyEnvironmentResponse'];
         };
       };
       401: components['responses']['UnauthorizedError'];
@@ -191,6 +335,78 @@ export interface operations {
       200: {
         content: {
           'application/json': components['schemas']['CreateApiKeyResponse'];
+        };
+      };
+      401: components['responses']['UnauthorizedError'];
+      500: components['responses']['InternalServerError'];
+    };
+  };
+  /** Returns the api key details */
+  describeApiKey: {
+    parameters: {
+      path: {
+        accountId: components['parameters']['AccountId'];
+      };
+    };
+    /** @description Specify the api key */
+    requestBody: {
+      content: {
+        'application/json': components['schemas']['DescribeApiKeyRequestBody'];
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          'application/json': components['schemas']['DescribeApiKeyResponse'];
+        };
+      };
+      401: components['responses']['UnauthorizedError'];
+      500: components['responses']['InternalServerError'];
+    };
+  };
+  /** Deletes the api key */
+  deleteApiKey: {
+    parameters: {
+      path: {
+        accountId: components['parameters']['AccountId'];
+      };
+    };
+    /** @description Specify the api key and environment id */
+    requestBody: {
+      content: {
+        'application/json': components['schemas']['DeleteApiKeyRequestBody'];
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          'application/json': components['schemas']['DeleteApiKeyResponse'];
+        };
+      };
+      401: components['responses']['UnauthorizedError'];
+      500: components['responses']['InternalServerError'];
+    };
+  };
+  /** Returns all the api keys */
+  listApiKeys: {
+    parameters: {
+      path: {
+        accountId: components['parameters']['AccountId'];
+      };
+    };
+    /** @description Specify the environment id */
+    requestBody: {
+      content: {
+        'application/json': components['schemas']['ListApiKeysRequestBody'];
+      };
+    };
+    responses: {
+      /** @description OK */
+      200: {
+        content: {
+          'application/json': components['schemas']['ListApiKeysResponse'];
         };
       };
       401: components['responses']['UnauthorizedError'];
