54a9e9559d3e0bcf198e65c048235da24673c041
Nik Nasr <nik@restate.dev>
2024-04-09 16:46:45 +0100

    update validator

--- libs/data-access/cloud-api-client/src/lib/validators.ts
+++ libs/data-access/cloud-api-client/src/lib/validators.ts
@@ -2,5 +2,10 @@ import { type components } from './api';
 
 type Role = components['schemas']['Role'];
 export function isRole(param: any): param is Role {
-  return ['rst:role::FullAccess'].includes(param);
+  return [
+    'rst:role::FullAccess',
+    'rst:role::IngressAccess',
+    'rst:role::AdminAccess',
+    'rst:role::ResolveAwakeableAccess',
+  ].includes(param);
 }
