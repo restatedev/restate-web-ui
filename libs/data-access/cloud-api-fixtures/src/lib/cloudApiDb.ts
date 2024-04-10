462f3139ae9359f7c5a82e34b6bfb545a08624d8
Nik Nasr <nik@restate.dev>
2024-04-10 22:24:33 +0100

    add mock handlers

--- libs/data-access/cloud-api-fixtures/src/lib/cloudApiDb.ts
+++ libs/data-access/cloud-api-fixtures/src/lib/cloudApiDb.ts
@@ -10,28 +10,49 @@ export const cloudApiDb = factory({
   account: {
     accountId: primaryKey(faker.string.uuid),
     users: manyOf('user'),
+    description: () => faker.internet.domainWord(),
   },
   environment: {
     environmentId: primaryKey(faker.string.uuid),
     account: oneOf('account'),
+    description: () => faker.commerce.product(),
   },
   apiKey: {
     account: oneOf('account'),
     environment: oneOf('environment'),
     keyId: () => faker.string.uuid(),
     apiKey: primaryKey(faker.string.uuid),
-    roleId: () => faker.helpers.arrayElement(['rst:role::FullAccess'] as const),
-    state: () => faker.helpers.arrayElement(['KEY_ACTIVE'] as const),
+    roleId: () =>
+      faker.helpers.arrayElement([
+        'rst:role::FullAccess',
+        'rst:role::IngressAccess',
+        'rst:role::AdminAccess',
+        'rst:role::ResolveAwakeableAccess',
+      ] as const),
+    state: () =>
+      faker.helpers.arrayElement([
+        'KEY_NEW',
+        'KEY_ACTIVE',
+        'KEY_DISABLED',
+        'KEY_DELETED',
+        'UNRECOGNIZED',
+      ] as const),
   },
 });
 
 const users = Array(5)
   .fill(null)
   .map(() => cloudApiDb.user.create());
-const account = cloudApiDb.account.create({ users });
+const account = cloudApiDb.account.create({
+  users,
+});
 const environments = Array(2)
   .fill(null)
-  .map(() => cloudApiDb.environment.create({ account }));
+  .map(() =>
+    cloudApiDb.environment.create({
+      account,
+    })
+  );
 environments.forEach((environment) => {
   cloudApiDb.apiKey.create({
     environment,
