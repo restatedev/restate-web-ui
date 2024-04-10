5636bd4bac9adf5eefe0116fc58d7c0facdeecd6
Nik Nasr <nik@restate.dev>
2024-04-11 00:39:11 +0100

    add db persistence

--- libs/data-access/cloud-api-fixtures/src/lib/cloudApiDb.ts
+++ libs/data-access/cloud-api-fixtures/src/lib/cloudApiDb.ts
@@ -1,5 +1,6 @@
 import { factory, manyOf, oneOf, primaryKey } from '@mswjs/data';
 import { faker } from '@faker-js/faker';
+import { DATABASE_INSTANCE } from '@mswjs/data/lib/glossary';
 
 faker.seed(123);
 
@@ -40,22 +41,100 @@ export const cloudApiDb = factory({
   },
 });
 
-const users = Array(5)
-  .fill(null)
-  .map(() => cloudApiDb.user.create());
-const account = cloudApiDb.account.create({
-  users,
+const DB_DATA = 'db';
+const getRawDbData = () => ({
+  users: cloudApiDb.user.getAll(),
+  accounts: cloudApiDb.account.getAll(),
+  environments: cloudApiDb.environment.getAll(),
+  apiKeys: cloudApiDb.apiKey.getAll(),
 });
-const environments = Array(2)
-  .fill(null)
-  .map(() =>
-    cloudApiDb.environment.create({
-      account,
+const persistDbData = () => {
+  setTimeout(() => {
+    localStorage.setItem(DB_DATA, JSON.stringify(getRawDbData()));
+  });
+};
+
+type RawDbData = ReturnType<typeof getRawDbData>;
+const dbInstance = cloudApiDb[DATABASE_INSTANCE];
+
+const persistedDbData = localStorage.getItem(DB_DATA);
+if (persistedDbData) {
+  const { users, accounts, environments, apiKeys } = JSON.parse(
+    persistedDbData
+  ) as RawDbData;
+  users.map((user) => cloudApiDb.user.create({ userId: user.userId }));
+  accounts.map((account) =>
+    cloudApiDb.account.create({
+      accountId: account.accountId,
+      description: account.description,
+      users: cloudApiDb.user.findMany({
+        where: {
+          userId: {
+            in: account.users.map(({ userId }) => userId),
+          },
+        },
+      }),
     })
   );
-environments.forEach((environment) => {
-  cloudApiDb.apiKey.create({
-    environment,
-    account: environment.account,
+  environments.map((environment) =>
+    cloudApiDb.environment.create({
+      environmentId: environment.environmentId,
+      description: environment.description,
+      account: cloudApiDb.account.findFirst({
+        where: {
+          accountId: {
+            equals: environment.account?.accountId,
+          },
+        },
+      })!,
+    })
+  );
+  apiKeys.map((apiKey) =>
+    cloudApiDb.apiKey.create({
+      roleId: apiKey.roleId,
+      keyId: apiKey.keyId,
+      apiKey: apiKey.apiKey,
+      state: apiKey.state,
+      account: cloudApiDb.account.findFirst({
+        where: {
+          accountId: {
+            equals: apiKey.account?.accountId,
+          },
+        },
+      })!,
+      environment: cloudApiDb.environment.findFirst({
+        where: {
+          environmentId: {
+            equals: apiKey.environment?.environmentId,
+          },
+        },
+      })!,
+    })
+  );
+} else {
+  const users = Array(5)
+    .fill(null)
+    .map(() => cloudApiDb.user.create());
+  const account = cloudApiDb.account.create({
+    users,
   });
-});
+  const environments = Array(2)
+    .fill(null)
+    .map(() =>
+      cloudApiDb.environment.create({
+        account,
+      })
+    );
+  environments.forEach((environment) => {
+    cloudApiDb.apiKey.create({
+      environment,
+      account: environment.account,
+    });
+  });
+}
+
+dbInstance.events.removeAllListeners();
+dbInstance.events.on('create', persistDbData);
+dbInstance.events.on('delete', persistDbData);
+dbInstance.events.on('update', persistDbData);
+persistDbData();
