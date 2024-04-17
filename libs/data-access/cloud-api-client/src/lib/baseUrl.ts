79110dc9abb1ba132a28dcdd855b3de22f2d7cb1
Nik Nasr <nik@restate.dev>
2024-04-17 16:26:25 +0100

    fix not providing restate url

--- libs/data-access/cloud-api-client/src/lib/baseUrl.ts
+++ libs/data-access/cloud-api-client/src/lib/baseUrl.ts
@@ -1,2 +1,9 @@
-export const CLOUD_API_BASE_URL = new URL(process.env.RESTATE_CLOUD_API_URL)
-  .href;
+let url: URL | null = null;
+try {
+  url = new URL(process.env.RESTATE_CLOUD_API_URL);
+} catch (error) {
+  throw new Error(
+    'Please provide a valid cloud API url via RESTATE_CLOUD_API_URL'
+  );
+}
+export const CLOUD_API_BASE_URL = url.href;
