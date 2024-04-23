4daa18751f57b9effec19e8e42f8b534a3eadc34
Nik Nasr <nik@restate.dev>
2024-04-23 11:17:56 +0100

    remove

--- libs/util/cache/src/lib/util-cache.spec.ts
+++
@@ -1,7 +0,0 @@
-import { utilCache } from './util-cache';
-
-describe('utilCache', () => {
-  it('should work', () => {
-    expect(utilCache()).toEqual('util-cache');
-  });
-});
--- libs/util/cache/src/lib/util-cache.ts
+++
@@ -1,3 +0,0 @@
-export function utilCache(): string {
-  return 'util-cache';
-}
