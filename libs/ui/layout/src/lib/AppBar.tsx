ff1f944a7205593b014ecfe0659be6e2531e5b72
Nik Nasr <nik@restate.dev>
2024-05-02 11:41:55 +0100

    fix header animation

--- libs/ui/layout/src/lib/AppBar.tsx
+++ libs/ui/layout/src/lib/AppBar.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from 'react';
+import styles from './AppBar.module.css';
 
 interface AppBarProps {
   id?: string;
@@ -7,11 +8,9 @@ interface AppBarProps {
 export function AppBar(props: PropsWithChildren<AppBarProps>) {
   return (
     <header
-      className={
-        '[&:has(.header:empty)]:invisible [&:not(:has(.header:empty))]:animate-in [&:not(:has(.header:empty))]:slide-in-from-top duration-300 [&:has([data-variant="hidden"])]:invisible [&:has([data-variant="secondary"])]:shadow-none [&:has([data-variant="secondary"])]:border-none [&:has([data-variant="secondary"])]:bg-transparent [&:has([data-variant="secondary"])]:backdrop-blur-0 sticky top-3 sm:top-6 rounded-xl border z-40 flex flex-none flex-wrap items-stretch justify-between gap-4 backdrop-blur-xl backdrop-saturate-150 h-14 shadow-lg shadow-zinc-800/5 bg-gray-50/90'
-      }
+      className={`${styles.header} [&:has(>*>*)]:animate-in [&:has(>*>*)]:slide-in-from-top duration-300 [&:has([data-variant="hidden"])]:invisible [&:has([data-variant="secondary"])]:shadow-none [&:has([data-variant="secondary"])]:border-none [&:has([data-variant="secondary"])]:bg-transparent [&:has([data-variant="secondary"])]:backdrop-blur-0 sticky top-3 sm:top-6 rounded-xl border z-40 flex flex-none flex-wrap items-stretch justify-between gap-4 backdrop-blur-xl backdrop-saturate-150 h-14 shadow-lg shadow-zinc-800/5 bg-gray-50/90`}
     >
-      <div {...props} className="header m-[-1px] flex items-stretch"></div>
+      <div {...props} className="m-[-1px] flex items-stretch"></div>
     </header>
   );
 }
