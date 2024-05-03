0b85e50fe14f5ff5a7c7c12993c3693df290c50a
Nik Nasr <nik@restate.dev>
2024-05-03 14:26:21 +0100

    add nav

--- libs/ui/layout/src/lib/AppBar.tsx
+++ libs/ui/layout/src/lib/AppBar.tsx
@@ -10,7 +10,7 @@ export function AppBar(props: PropsWithChildren<AppBarProps>) {
     <header
       className={`${styles.header} [&:has(>*>*)]:animate-in [&:has(>*>*)]:slide-in-from-top duration-300 [&:has([data-variant="hidden"])]:invisible [&:has([data-variant="secondary"])]:shadow-none [&:has([data-variant="secondary"])]:border-none [&:has([data-variant="secondary"])]:bg-transparent [&:has([data-variant="secondary"])]:backdrop-blur-0 sticky top-3 sm:top-6 rounded-xl border z-40 flex flex-none flex-wrap items-stretch justify-between gap-4 backdrop-blur-xl backdrop-saturate-150 h-14 shadow-lg shadow-zinc-800/5 bg-gray-50/90`}
     >
-      <div {...props} className="m-[-1px] flex items-stretch"></div>
+      <div {...props} className="m-[-1px] flex items-stretch flex-1"></div>
     </header>
   );
 }
