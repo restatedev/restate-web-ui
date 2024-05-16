cc7fc4bcb4effa6c997d4de6b723b23b12c3d991
Nik <nik@restate.dev>
2024-05-16 16:21:00 +0100

    Add ingress and admin urls to settings page (#29)

--- libs/features/cloud-settings-route/src/lib/settings.route.tsx
+++ libs/features/cloud-settings-route/src/lib/settings.route.tsx
@@ -4,6 +4,7 @@ import {
   defer,
   Await,
   useAsyncValue,
+  useRouteLoaderData,
 } from '@remix-run/react';
 import {
   describeApiKey,
@@ -20,6 +21,7 @@ import { ErrorBanner } from '@restate/ui/error';
 import { Button } from '@restate/ui/button';
 import { Icon, IconName } from '@restate/ui/icons';
 import { useEnvironmentParam } from '@restate/features/cloud/routes-utils';
+import { environments } from '@restate/features/cloud/environments-route';
 
 const clientLoader = async ({ request, params }: ClientLoaderFunctionArgs) => {
   const accountId = params.accountId;
@@ -53,13 +55,13 @@ const clientLoader = async ({ request, params }: ClientLoaderFunctionArgs) => {
   });
 };
 
-function Component() {
-  const { apiKeysWithDetailsPromises } = useLoaderData<typeof clientLoader>();
+function CLI() {
   const [isCopied, setIsCopied] = useState(false);
   const environmentId = useEnvironmentParam();
+  invariant(environmentId, 'Missing environmentId param');
 
   return (
-    <div className="grid gap-x-10 gap-y-4 sm:grid-cols-[20ch_1fr]">
+    <>
       <div>
         <h2 className="text-base font-semibold leading-7 text-gray-900">CLI</h2>
         <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -76,7 +78,7 @@ function Component() {
               <span className="text-blue-700">export</span>{' '}
               <span className="">RESTATE_HOST</span>=
               <span className="text-red-700">
-                {environmentId!.split('env_')[1]}
+                {environmentId.split('env_')[1]}
                 .env.dev.restate.cloud
               </span>
             </span>
@@ -96,7 +98,7 @@ function Component() {
             onClick={() => {
               navigator.clipboard.writeText(
                 `export RESTATE_HOST=${
-                  environmentId!.split('env_')[1]
+                  environmentId.split('env_')[1]
                 }.env.dev.restate.cloud\nexport RESTATE_HOST_SCHEME=https\nrestate whoami`
               );
               setIsCopied(true);
@@ -113,12 +115,125 @@ function Component() {
           </Button>
         </div>
       </div>
+    </>
+  );
+}
+
+function Http() {
+  const environmentId = useEnvironmentParam();
+
+  invariant(environmentId, 'Missing environmentId param');
+  const environmentsResponse = useRouteLoaderData<
+    typeof environments.clientLoader
+  >('routes/accounts.$accountId.environments');
+  const environmentDetailsPromise =
+    environmentsResponse?.environmentsWithDetailsPromises[environmentId];
+  const [isIngressopied, setIngressIsCopied] = useState(false);
+  const [isAdminCopied, setAdminIsCopied] = useState(false);
+
+  return (
+    <>
+      <div className="mt-10">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">
+          HTTP
+        </h2>
+        <p className="mt-1 text-sm leading-6 text-gray-600">
+          To invoke your handlers or manage your services, deployments, and
+          invocations in this environment over HTTP, please use this URLs.
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 sm:mt-10">
+        <div className="flex flex-col gap-2 items-stretch font-mono [overflow-wrap:anywhere] rounded-xl border bg-gray-200/50 shadow-[inset_0_1px_0px_0px_rgba(0,0,0,0.03)] text-xs p-2">
+          <Suspense>
+            <Await resolve={environmentDetailsPromise}>
+              {(environmentDetails) => (
+                <>
+                  <div className="flex gap-2 items-start">
+                    <code className="flex-auto px-2 self-center flex gap-2 flex-col py-2">
+                      <span className="text-green-800">
+                        # Here is your ingress URL for invoking your handlers
+                        (example use case):
+                      </span>
+                      <span>
+                        {`curl ${environmentDetails?.data?.ingressBaseUrl}/MyService/MyHandler -H "Authorization: Bearer $My_API_KEY"`}
+                      </span>
+                    </code>
+                    <Button
+                      variant="icon"
+                      className="flex-shrink-0 flex items-center gap-1 p-2"
+                      onClick={() => {
+                        navigator.clipboard.writeText(
+                          environmentDetails?.data?.ingressBaseUrl ?? ''
+                        );
+                        setIngressIsCopied(true);
+                        setTimeout(() => {
+                          setIngressIsCopied(false);
+                        }, 1000);
+                      }}
+                    >
+                      {isIngressopied ? (
+                        <Icon name={IconName.Check} />
+                      ) : (
+                        <Icon name={IconName.Copy} />
+                      )}
+                    </Button>
+                  </div>
+                  <div className="flex gap-2 items-start">
+                    <code className="flex-auto px-2 self-center flex gap-2 flex-col py-2">
+                      <span className="text-green-800">
+                        # Here is your Admin API URL for managing services,
+                        deployments and invocations (example use case):
+                      </span>
+                      <span>
+                        {`curl ${environmentDetails?.data?.adminBaseUrl}/deployments -H "Authorization: Bearer $My_API_KEY"`}
+                      </span>
+                    </code>
+                    <Button
+                      variant="icon"
+                      className="flex-shrink-0 flex items-center gap-1 p-2"
+                      onClick={() => {
+                        navigator.clipboard.writeText(
+                          environmentDetails?.data?.adminBaseUrl ?? ''
+                        );
+                        setAdminIsCopied(true);
+                        setTimeout(() => {
+                          setAdminIsCopied(false);
+                        }, 1000);
+                      }}
+                    >
+                      {isAdminCopied ? (
+                        <Icon name={IconName.Check} />
+                      ) : (
+                        <Icon name={IconName.Copy} />
+                      )}
+                    </Button>
+                  </div>
+                </>
+              )}
+            </Await>
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Component() {
+  const { apiKeysWithDetailsPromises } = useLoaderData<typeof clientLoader>();
+  const environmentId = useEnvironmentParam();
+  invariant(environmentId, 'Missing environmentId param');
+
+  return (
+    <div className="grid gap-x-10 gap-y-4 sm:grid-cols-[20ch_1fr]">
+      <CLI />
+      <Http />
       <div className="mt-10">
         <h2 className="text-base font-semibold leading-7 text-gray-900">
           API keys
         </h2>
         <p className="mt-1 text-sm leading-6 text-gray-600">
-          You will need an API key to interact with your restate Cloud instance.
+          To interact with your restate Cloud instance using methods other than
+          the restate CLI, you will need an API key.
         </p>
       </div>
       <div className="flex flex-col gap-2 sm:mt-10">
