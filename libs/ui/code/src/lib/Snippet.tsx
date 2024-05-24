ba6096b1cc44988fd9b8757ec2b64b9b765facdc
Nik Nasr <nik@restate.dev>
2024-05-24 19:04:42 +0100

    fix styles

--- libs/ui/code/src/lib/Snippet.tsx
+++ libs/ui/code/src/lib/Snippet.tsx
@@ -1,26 +1,57 @@
 import { Button } from '@restate/ui/button';
 import { Icon, IconName } from '@restate/ui/icons';
-import { PropsWithChildren, useState } from 'react';
+import { Children, PropsWithChildren, memo, useState } from 'react';
 import { tv } from 'tailwind-variants';
+import { syntaxHighlighter } from './SyntaxHighlighter';
 
 interface SnippetProps {
   className?: string;
+  language?: 'typescript' | 'java' | 'json' | 'bash';
 }
 
+function SyntaxHighlighter({
+  code,
+  language,
+}: {
+  code: string;
+  language: Exclude<SnippetProps['language'], undefined>;
+}) {
+  return (
+    <span
+      className="py-1 group-has-[.copy]:py-2"
+      dangerouslySetInnerHTML={{
+        __html: syntaxHighlighter.highlight(code, {
+          language,
+          ignoreIllegals: false,
+        }).value,
+      }}
+    />
+  );
+}
+
+const OptimizedSyntaxHighlighter = memo(SyntaxHighlighter);
+
 const snippetStyles = tv({
-  base: 'flex gap-2 items-start p-2 pb-0 [&:not(:has(.copy))]:group-has-[.copy]:pr-16 [&_.copy]:-mt-2 [&_.copy]:-mr-2 [&_[data-comment]]:text-green-800 [&_[data-shell-command]]:text-red-700',
+  base: 'flex gap-2 gap-x-2 items-start group p-2 py-0 has-[.copy]:-my-1 has-[.copy]:pr-1 [&:not(:has(.copy))]:group-has-[.copy]/code:pr-16 [&_.copy2]:-mt-2 [&_.copy]:-mr-2',
 });
 export function Snippet({
   children,
   className,
+  language = 'bash',
 }: PropsWithChildren<SnippetProps>) {
+  const childrenArray = Children.toArray(children);
+  const codes = childrenArray
+    .filter((child) => typeof child === 'string')
+    .join('');
+  const others = childrenArray.filter((child) => typeof child !== 'string');
   return (
     <span
       className={snippetStyles({
         className,
       })}
     >
-      {children}
+      <OptimizedSyntaxHighlighter language={language} code={codes} />
+      {others}
     </span>
   );
 }
@@ -31,7 +62,7 @@ interface SnippetCopyProps {
 }
 
 const snippetCopyStyles = tv({
-  base: 'copy flex-shrink-0 flex items-center gap-1 p-2 ml-auto',
+  base: 'copy flex-shrink-0 flex items-center gap-1 ml-auto p-2 ',
 });
 export function SnippetCopy({
   className,
