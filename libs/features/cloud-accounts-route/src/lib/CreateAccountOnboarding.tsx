8e0452f89d55e2a67211fa8a9b095c14dc2d7755
Nik Nasr <nik@restate.dev>
2024-06-01 11:43:13 +0100

    update t&c

--- libs/features/cloud-accounts-route/src/lib/CreateAccountOnboarding.tsx
+++ libs/features/cloud-accounts-route/src/lib/CreateAccountOnboarding.tsx
@@ -14,7 +14,7 @@ export function CreateAccountOnboarding() {
       <fetcher.Form
         method="POST"
         action={action}
-        className="flex flex-col gap-4 bg-white rounded-xl border p-5 max-w-md mx-auto"
+        className="flex flex-col gap-6 bg-white rounded-xl border p-5 max-w-md mx-auto"
       >
         <h3 className="text-lg font-medium leading-6 text-gray-900">
           Welcome to restate Cloud!
@@ -29,7 +29,7 @@ export function CreateAccountOnboarding() {
             autoFocus
             required
             name="name"
-            className="mt-2"
+            className="mt-4"
             placeholder="Name"
             pattern="[a-z0-9][a-z0-9\-]{0,61}[a-z0-9]"
           />
@@ -55,6 +55,30 @@ export function CreateAccountOnboarding() {
               </Link>
             </span>
           </FormFieldCheckbox>
+          <p className="text-gray-500 mt-4 text-xs">
+            As part of the use of our services, we may send our customers
+            product information and updates on our offerings. You can object to
+            this at any time and free of charge by sending an email to{' '}
+            <Link
+              href="mailto:dataprotection@restate.dev"
+              target="_blank"
+              variant="secondary"
+              rel="noreferrer noopener"
+            >
+              dataprotection@restate.dev
+            </Link>
+            . Further information on the processing of your personal data can be
+            found at{' '}
+            <Link
+              href="https://restate.dev/privacy/"
+              target="_blank"
+              variant="secondary"
+              rel="noreferrer noopener"
+            >
+              https://restate.dev/privacy/
+            </Link>
+            .
+          </p>
         </div>
         <ErrorBanner errors={fetcher.errors} />
         <SubmitButton variant="primary" className="flex-auto">
