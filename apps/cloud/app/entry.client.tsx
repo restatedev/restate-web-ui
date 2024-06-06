9948dcd9481590c77b7826b0a3acef675deb7a23
Nik Nasr <nik@restate.dev>
2024-06-06 19:51:56 +0100

    add ssr

--- apps/cloud/app/entry.client.tsx
+++ apps/cloud/app/entry.client.tsx
@@ -1,26 +1,12 @@
 import { RemixBrowser } from '@remix-run/react';
-import { enableMockApi } from '@restate/util/mock-api';
 import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
-async function prepareApp() {
-  if (process.env.NODE_ENV === 'development' && process.env.MOCK) {
-    const { cloudApiMockHandlers } = await import(
-      '@restate/data-access/cloud/api-fixtures'
-    );
-    return await enableMockApi(cloudApiMockHandlers);
-  }
-
-  return Promise.resolve();
-}
-
-prepareApp().then(() => {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  });
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
+  );
 });
