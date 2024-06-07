e2919b05c6f4d69b3a890f621ee102e8cb7547d4
Nik Nasr <nik@restate.dev>
2024-06-07 13:52:10 +0100

    add types

--- @types/load-context.d.ts
+++ @types/load-context.d.ts
@@ -1,4 +1,4 @@
-import { type PlatformProxy } from "wrangler";
+import { type PlatformProxy } from 'wrangler';
 
 // When using `wrangler.toml` to configure bindings,
 // `wrangler types` will generate types for those bindings
@@ -6,11 +6,13 @@ import { type PlatformProxy } from "wrangler";
 // Need this empty interface so that typechecking passes
 // even if no `wrangler.toml` exists.
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Env {}
+interface Env {
+  SLACK_TOKEN: string;
+}
 
-type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
+type Cloudflare = Omit<PlatformProxy<Env>, 'dispose'>;
 
-declare module "@remix-run/cloudflare" {
+declare module '@remix-run/cloudflare' {
   interface AppLoadContext {
     cloudflare: Cloudflare;
   }
