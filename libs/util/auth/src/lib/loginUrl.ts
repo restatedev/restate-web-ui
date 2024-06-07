071b5d1d71d72c148769429826401708ed51cb24
Nik Nasr <nik@restate.dev>
2024-06-07 16:29:31 +0100

    update login url

--- libs/util/auth/src/lib/loginUrl.ts
+++ libs/util/auth/src/lib/loginUrl.ts
@@ -1,15 +1,10 @@
 import { getAuthRedirectUri } from './authRedirectUri';
 
 export function getLoginUrl(url: URL) {
-  const redirectUrl = new URL(window.location.href);
-  redirectUrl.pathname = '/auth';
-  redirectUrl.search = '';
   url = new URL(
     `${process.env.RESTATE_AUTH_URL}/login?client_id=${
       process.env.RESTATE_AUTH_CLIENT_ID
-    }&response_type=code&redirect_uri=${
-      getAuthRedirectUri(new URL(window.location.href)).href
-    }`
+    }&response_type=code&redirect_uri=${getAuthRedirectUri(url).href}`
   );
 
   return url;
