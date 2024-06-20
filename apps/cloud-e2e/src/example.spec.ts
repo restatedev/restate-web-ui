655d75c0e78de41bb1e99392adf7afbbd5550e02
Nik Nasr <nik@restate.dev>
2024-06-20 21:56:10 +0100

    fix e2e

--- apps/cloud-e2e/src/example.spec.ts
+++ apps/cloud-e2e/src/example.spec.ts
@@ -3,5 +3,7 @@ import { test } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await page.waitForURL(/\/accounts/);
+  await page.waitForURL((url) => {
+    return url.pathname.startsWith('/accounts');
+  });
 });
