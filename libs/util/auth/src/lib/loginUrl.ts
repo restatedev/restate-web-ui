22735536b4293cac6d4075220d2f28ebdde47a85
Nik Nasr <nik@restate.dev>
2024-08-02 14:23:31 +0100

    update login url

--- libs/util/auth/src/lib/loginUrl.ts
+++ libs/util/auth/src/lib/loginUrl.ts
@@ -1,10 +1,25 @@
-import { getAuthRedirectUri } from './authRedirectUri';
+import type { AppLoadContext } from '@remix-run/cloudflare';
 
-export function getLoginUrl(url: URL) {
-  url = new URL(
-    `${process.env['RESTATE_AUTH_URL']}/login?client_id=${
-      process.env['RESTATE_AUTH_CLIENT_ID']
-    }&response_type=code&redirect_uri=${getAuthRedirectUri(url).href}`
-  );
-  return url;
+export function getLoginURL({
+  RESTATE_AUTH_CLIENT_ID,
+  RESTATE_AUTH_URL,
+  RESTATE_AUTH_REDIRECT_URL,
+  url: _url,
+}: Pick<
+  AppLoadContext['cloudflare']['env'],
+  'RESTATE_AUTH_CLIENT_ID' | 'RESTATE_AUTH_URL' | 'RESTATE_AUTH_REDIRECT_URL'
+> & { url: URL }) {
+  const url = new URL(_url);
+  if (url.pathname === '/logout') {
+    url.pathname = '';
+    url.search = '';
+    url.hash = '';
+  }
+  return new URL(
+    `${RESTATE_AUTH_URL}/login?client_id=${RESTATE_AUTH_CLIENT_ID}&response_type=code&redirect_uri=${RESTATE_AUTH_REDIRECT_URL}&state=${encodeURI(
+      url.href
+    )
+      .split(`${url.protocol}//${url.host}`)
+      .at(-1)}`
+  ).href;
 }
