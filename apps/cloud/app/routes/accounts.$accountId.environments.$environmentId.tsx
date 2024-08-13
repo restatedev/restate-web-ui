6ed18dd8cb2ee9432a85cc9fbbf36eac5f960074
Nik Nasr <nik@restate.dev>
2024-08-13 21:49:53 +0100

    describe environment api

---
+++ apps/cloud/app/routes/accounts.$accountId.environments.$environmentId.tsx
@@ -0,0 +1,34 @@
+import { json } from '@remix-run/cloudflare';
+import { describeEnvironment } from '@restate/data-access/cloud/api-client';
+import { settings } from '@restate/features/cloud/settings-route';
+import { withCookieAuth } from '@restate/util/auth';
+import invariant from 'tiny-invariant';
+import { adminUrlCookie } from './adminCookie';
+
+export const loader = withCookieAuth(async ({ params, authToken }) => {
+  const { accountId, environmentId } = params;
+  invariant(accountId, 'Missing accountId param');
+  invariant(environmentId, 'Missing environmentId param');
+
+  const { data, error, response } = await describeEnvironment({
+    accountId,
+    environmentId,
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+
+  if (error) {
+    return new Response(JSON.stringify(error), { status: response.status });
+  }
+
+  return json(data, {
+    headers: {
+      'Set-Cookie': await adminUrlCookie.serialize(data?.adminBaseUrl, {
+        path: `/api/accounts/${accountId}/environments/${environmentId}/admin`,
+      }),
+    },
+  });
+});
+export const clientAction = settings.clientAction;
+export default settings.Component;
