9e02844bc2b6a6670fa75123b006053283c35965
Nik Nasr <nik@restate.dev>
2024-08-16 16:52:00 +0100

    add more api

---
+++ apps/cloud/app/routes/api.accounts.$accountId.environments.$environmentId.keys.$keyId.tsx
@@ -0,0 +1,76 @@
+import {
+  ActionFunctionArgs,
+  json,
+  LoaderFunctionArgs,
+} from '@remix-run/cloudflare';
+import {
+  deleteApiKey,
+  describeApiKey,
+} from '@restate/data-access/cloud/api-client';
+import invariant from 'tiny-invariant';
+
+export const action = async ({
+  request,
+  params,
+  context,
+}: ActionFunctionArgs) => {
+  const accountId = params.accountId;
+  const environmentId = params.environmentId;
+  const keyId = params.keyId;
+  invariant(accountId, 'Missing accountId param');
+  invariant(environmentId, 'Missing environmentId param');
+  invariant(keyId, 'Missing keyId param');
+
+  switch (request.method) {
+    case 'DELETE': {
+      const { data, error, response } = await deleteApiKey({
+        accountId,
+        environmentId,
+        keyId,
+        headers: request.headers,
+      });
+
+      if (data) {
+        return json({});
+      } else {
+        return new Response(JSON.stringify(error), { status: response.status });
+      }
+    }
+
+    default:
+      return json({}, { status: 501 });
+  }
+};
+
+export const loader = async ({
+  request,
+  params,
+  context,
+}: LoaderFunctionArgs) => {
+  const accountId = params.accountId;
+  const environmentId = params.environmentId;
+  const keyId = params.keyId;
+  invariant(accountId, 'Missing accountId param');
+  invariant(environmentId, 'Missing environmentId param');
+  invariant(keyId, 'Missing keyId param');
+
+  switch (request.method) {
+    case 'GET': {
+      const { data, response, error } = await describeApiKey({
+        accountId,
+        environmentId,
+        keyId,
+        headers: request.headers,
+      });
+
+      if (data) {
+        return json(data);
+      } else {
+        return new Response(JSON.stringify(error), { status: response.status });
+      }
+    }
+
+    default:
+      return new Response('', { status: 501 });
+  }
+};
