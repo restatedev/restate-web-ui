c7bcf8ca9232c9aab9c7110798bdfacaa93012c2
Nik <nik@restate.dev>
2024-08-29 17:05:47 +0100

    Web UI skeleton app (#57)

---
+++ apps/web-ui/app/routes/_index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <h1 className="red-500">Web UI</h1>
+    </div>
+  );
+}
---
+++ apps/web-ui/tailwind.config.cjs
@@ -0,0 +1,33 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+const defaultTheme = require('tailwindcss/defaultTheme');
+const { withTV } = require('tailwind-variants/transformer');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withTV({
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'selector',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        '2xs': '0.6875rem',
+        code: '0.8125rem',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-react-aria-components'),
+    require('tailwindcss-animate'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/container-queries'),
+  ],
+});
