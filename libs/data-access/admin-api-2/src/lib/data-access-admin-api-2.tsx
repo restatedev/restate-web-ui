ba1b484f3cc47a80a943875c9aa8932c4a782fbd
Nik Nasr <nik@restate.dev>
2024-08-30 10:31:12 +0100

    add lib

---
+++ libs/data-access/admin-api-2/src/lib/data-access-admin-api-2.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledDataAccessAdminApi2 = styled.div`
+  color: pink;
+`;
+
+export function DataAccessAdminApi2() {
+  return (
+    <StyledDataAccessAdminApi2>
+      <h1>Welcome to DataAccessAdminApi2!</h1>
+    </StyledDataAccessAdminApi2>
+  );
+}
+
+export default DataAccessAdminApi2;
---
+++ libs/data-access/admin-api-2/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../../node_modules/.vite/libs/data-access/admin-api-2',
+
+  plugins: [react(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../../coverage/libs/data-access/admin-api-2',
+      provider: 'v8',
+    },
+  },
+});
