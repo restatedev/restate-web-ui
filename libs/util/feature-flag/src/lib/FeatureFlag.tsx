09b368cbea7bec1c0f7c4f4bf622504a7f7d8ad2
Nik Nasr <nik@restate.dev>
2024-09-02 14:29:30 +0100

    add feature to overview

--- libs/util/feature-flag/src/lib/FeatureFlag.tsx
+++ libs/util/feature-flag/src/lib/FeatureFlag.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from 'react';
 import { hasAccessToFeature } from './hasAccessToFeature';
+import type { FeatureFlag } from './type';
 
 export function FeatureFlag({
   featureFlag,
   children,
-}: PropsWithChildren<{ featureFlag: string }>) {
+}: PropsWithChildren<{ featureFlag: FeatureFlag }>) {
   if (hasAccessToFeature(featureFlag)) {
     return children;
   }
--- libs/util/feature-flag/src/lib/withFeatureFlag.ts
+++ libs/util/feature-flag/src/lib/withFeatureFlag.ts
@@ -1,17 +1,18 @@
 import { LoaderFunction } from '@remix-run/cloudflare';
 import { ClientLoaderFunction, redirect } from '@remix-run/react';
 import { hasAccessToFeature } from './hasAccessToFeature';
+import type { FeatureFlag } from './type';
 
 export function withFeatureFlag(
-  featureFlag: string,
+  featureFlag: FeatureFlag,
   loader: LoaderFunction
 ): LoaderFunction;
 export function withFeatureFlag(
-  featureFlag: string,
+  featureFlag: FeatureFlag,
   loader: ClientLoaderFunction
 ): ClientLoaderFunction;
 export function withFeatureFlag(
-  featureFlag: string,
+  featureFlag: FeatureFlag,
   loader: LoaderFunction | ClientLoaderFunction
 ): LoaderFunction | ClientLoaderFunction {
   if (hasAccessToFeature(featureFlag)) {
