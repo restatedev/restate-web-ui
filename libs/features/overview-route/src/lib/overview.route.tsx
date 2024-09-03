2e7bd405e79c50ef1ff010d4816c0e6ee1697c5e
Nik Nasr <nik@restate.dev>
2024-09-03 13:41:58 +0100

    add restate server

--- libs/features/overview-route/src/lib/overview.route.tsx
+++ libs/features/overview-route/src/lib/overview.route.tsx
@@ -1,5 +1,13 @@
+import { RestateServer } from './RestateServer';
+
 function Component() {
-  return <h1>Overview</h1>;
+  return (
+    <div className="flex-auto grid gap-6 [grid-template-columns:1fr] md:[grid-template-columns:1fr_150px_1fr] items-center">
+      <div className=""></div>
+      <RestateServer className="hidden md:block" />
+      <div className=""></div>
+    </div>
+  );
 }
 
 export const overview = { Component };
