b46810eb2cd605eb4f3516b4e757dfc63b41c027
Nik Nasr <nik@restate.dev>
2024-09-14 19:23:48 +0330

    fix placeholder

--- libs/features/overview-route/src/lib/Deployment.tsx
+++ libs/features/overview-route/src/lib/Deployment.tsx
@@ -2,7 +2,7 @@ import { Deployment } from '@restate/data-access/admin-api';
 import { tv } from 'tailwind-variants';
 
 const styles = tv({
-  base: 'w-full h-[80px] bg-red-200',
+  base: 'w-full h-[80px] bg-white border shadow-sm p-2 rounded-lg h-[2rem] flex items-center text-sm',
 });
 export function Deployment({
   deployment,
@@ -12,13 +12,16 @@ export function Deployment({
   className?: string;
 }) {
   return (
-    <div
-      className={styles({ className })}
-      style={{
-        height: `${Math.random() * 200 + 50}px`,
-      }}
-    >
-      deployment
+    <div className={className}>
+      <div className={styles()}>Deployment</div>
+      {deployment.services.map((service, i) => (
+        <div
+          key={i}
+          className={styles({ className: 'mx-1 w-[calc(100%-0.25rem)]' })}
+        >
+          Service {service.name}
+        </div>
+      ))}
     </div>
   );
 }
