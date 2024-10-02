3a86a76264b1548169bf57f5e1f29e0593f5cb09
Nik Nasr <nik@restate.dev>
2024-10-02 09:04:21 +0100

    add service

--- libs/features/overview-route/src/lib/Deployment.tsx
+++ libs/features/overview-route/src/lib/Deployment.tsx
@@ -2,6 +2,7 @@ import type { Deployment } from '@restate/data-access/admin-api';
 import { Icon, IconName } from '@restate/ui/icons';
 import { tv } from 'tailwind-variants';
 import { isHttpDeployment } from './types';
+import { Service } from './Service';
 
 const styles = tv({
   base: 'w-full rounded-xl border bg-gray-200/50 shadow-[inset_0_1px_0px_0px_rgba(0,0,0,0.03)]',
@@ -49,22 +50,6 @@ export function Deployment({
           ))}
         </div>
       )}
-    </div>
-  );
-}
-
-function Service({ service }: { service: Deployment['services'][number] }) {
-  return (
-    <div className="flex flex-row items-center gap-2 pr-4 bg-gray-50 shadow-sm border rounded-[calc(0.75rem-1px)]">
-      <div className="h-9 aspect-square p-[2px]">
-        <div className="rounded-[calc(0.75rem-2px)] bg-gray-200/40 h-full w-full flex items-center justify-center text-gray-400">
-          <Icon name={IconName.Box} className="w-5 h-5" />
-        </div>
-      </div>
-      <div className="text-code">{service.name}</div>
-      <div className="rounded-full text-xs bg-white border px-2 py-0.5 ring-1 ring-inset ring-gray-100 text-gray-500">
-        rev. {service.revision}
-      </div>
     </div>
   );
 }
