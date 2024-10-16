0e1e0dc4d25c98f86751093ea5077e8a8a7527fc
Nik Nasr <nik@restate.dev>
2024-10-16 16:59:26 +0100

    upgrade eslint
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ apps/web-ui/eslint.config.cjs
@@ -0,0 +1,21 @@
+const baseConfig = require('../../eslint.config.js');
+
+module.exports = [
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  { ignores: ['build', 'public/build'] },
+];
