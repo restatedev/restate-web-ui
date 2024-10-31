c592e217eb4dad49cf63b6a9c468de5ab2b99463
Nik Nasr <nik@restate.dev>
2024-10-31 12:58:02 +0000

    update style
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/tooltip/src/lib/TooltipContent.tsx
+++ libs/ui/tooltip/src/lib/TooltipContent.tsx
@@ -49,16 +49,18 @@ export function InternalTooltipContent({
         styles({ ...renderProps, className, small })
       )}
     >
-      <OverlayArrow>
-        <svg
-          width={8}
-          height={8}
-          viewBox="0 0 8 8"
-          className="fill-zinc-800 stroke-zinc-900/80 group-placement-bottom:rotate-180 group-placement-left:-rotate-90 group-placement-right:rotate-90"
-        >
-          <path d="M0 0 L4 4 L8 0" />
-        </svg>
-      </OverlayArrow>
+      {small && (
+        <OverlayArrow>
+          <svg
+            width={8}
+            height={8}
+            viewBox="0 0 8 8"
+            className="fill-zinc-800 stroke-zinc-900/80 group-placement-bottom:rotate-180 group-placement-left:-rotate-90 group-placement-right:rotate-90"
+          >
+            <path d="M0 0 L4 4 L8 0" />
+          </svg>
+        </OverlayArrow>
+      )}
       {children}
     </AriaTooltip>
   );
