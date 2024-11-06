25020e179b2ff9550d45fb95ffae148ff34d94b8
Nik Nasr <nik@restate.dev>
2024-11-06 17:04:59 +0000

    fix baseurl
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- apps/web-ui/vite.config.ts
+++ apps/web-ui/vite.config.ts
@@ -2,7 +2,7 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig, loadEnv } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-const BASE_URL = '/ui';
+const BASE_URL = '/ui/';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
