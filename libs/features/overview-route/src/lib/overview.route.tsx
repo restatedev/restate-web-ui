8e4c02ae19b649c5c3aa0d5c9b97c88f133061ed
Nik <nik@restate.dev>
2024-11-07 16:18:38 +0000

    Update overview layout and fix few styling issue (#88)
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/overview-route/src/lib/overview.route.tsx
+++ libs/features/overview-route/src/lib/overview.route.tsx
@@ -2,49 +2,20 @@ import { useListDeployments } from '@restate/data-access/admin-api';
 import { RestateServer } from './RestateServer';
 import { tv } from 'tailwind-variants';
 import { TriggerRegisterDeploymentDialog } from './RegisterDeployment/Dialog';
-import { ServiceDetails } from './Details.tsx/Service';
-import { DeploymentDetails } from './Details.tsx/Deployment';
 import {
   ServiceDeploymentExplainer,
   ServiceExplainer,
 } from '@restate/features/explainers';
 import { Service } from './Service';
-
-const deploymentsStyles = tv({
-  base: 'w-full md:row-start-1 md:col-start-1 grid gap-8 gap-x-20 gap2-x-[calc(8rem+150px)]',
-  variants: {
-    isEmpty: {
-      true: 'gap-0 h-full [grid-template-columns:1fr]',
-      false:
-        'h-fit [grid-template-columns:1fr] md:[grid-template-columns:1fr_150px_1fr]',
-    },
-  },
-  defaultVariants: {
-    isEmpty: false,
-  },
-});
-const reactServerStyles = tv({
-  base: 'justify-center flex md:sticky md:top-[11rem] flex-col items-center w-fit',
-  variants: {
-    isEmpty: {
-      true: 'md:h-[calc(100vh-150px-6rem)] py-8 flex-auto w-full justify-center rounded-xl border bg-gray-200/50 shadow-[inset_0_1px_0px_0px_rgba(0,0,0,0.03)]',
-      false:
-        'h-fit md:max-h-[calc(100vh-150px-6rem)] min-h-[min(100%,calc(100vh-150px-6rem))]',
-    },
-  },
-  defaultVariants: {
-    isEmpty: false,
-  },
-});
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { useLayoutEffect, useState } from 'react';
 
 function MultipleDeploymentsPlaceholder() {
   return (
-    <div className="flex flex-col gap-2 items-center relative w-full text-center mt-6">
-      <div className="mt-4">
-        <TriggerRegisterDeploymentDialog>
-          Deployment
-        </TriggerRegisterDeploymentDialog>
-      </div>
+    <div className="[&_a]:px-5 [&_a]:py-1.5 [&_a]:rounded-xl [&_a]:shadow-[inset_0_1px_0_0_theme(colors.gray.500)] [&_a]:[filter:drop-shadow(0_8px_6px_rgb(39_39_42/0.15))_drop-shadow(0_4px_3px_rgb(39_39_42/0.2))] [&_a:hover]:[filter:drop-shadow(0_12px_9px_rgb(39_39_42/0.15))_drop-shadow(0_8px_6px_rgb(39_39_42/0.2))] [&_a]:backdrop-blur-xl [&_a]:text-zinc-200 [&_a]:border-zinc-900/80 [&_a]:bg-zinc-900/90 [&_a:hover]:bg-zinc-900/85 [&_a[data-pressed=true]]:bg-zinc-900/80 fixed bottom-[2.5rem] left-[50vw] -translate-x-1/2 flex flex-col gap-2 items-center text-center [&_a:hover]:scale-105 [&_a[data-pressed=true]]:scale-100 will-change-transform">
+      <TriggerRegisterDeploymentDialog>
+        Deployment
+      </TriggerRegisterDeploymentDialog>
     </div>
   );
 }
@@ -87,6 +58,45 @@ function NoDeploymentPlaceholder() {
   );
 }
 
+const deploymentsStyles = tv({
+  base: '',
+  variants: {
+    isEmpty: {
+      true: 'hidden',
+      false: 'min-h-full',
+    },
+  },
+  defaultVariants: {
+    isEmpty: false,
+  },
+});
+const layoutStyles = tv({
+  base: 'min-h-full [&>*]:items-center',
+  variants: {
+    isScrolling: {
+      true: 'items-start',
+      false: 'items-center',
+    },
+  },
+  defaultVariants: {
+    isScrolling: false,
+  },
+});
+
+const reactServerStyles = tv({
+  base: 'justify-center flex flex-col items-center w-fit',
+  variants: {
+    isEmpty: {
+      true: 'mb-[-6rem] pb-8 pt-24 flex-auto w-full justify-center rounded-xl border bg-gray-200/50 shadow-[inset_0_1px_0px_0px_rgba(0,0,0,0.03)]',
+      false:
+        'hidden lg:block lg:fixed top-[50vh] left-[50vw] -translate-y-1/2 -translate-x-1/2',
+    },
+  },
+  defaultVariants: {
+    isEmpty: false,
+  },
+});
+
 // TODO: refactor layout
 function Component() {
   const {
@@ -96,39 +106,49 @@ function Component() {
   } = useListDeployments();
   const size = services ? services.size : 0;
   const isEmpty = isSuccess && (!deployments || deployments.size === 0);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useLayoutEffect(() => {
+    let isCanceled = false;
+    const resizeObserver = new ResizeObserver(() => {
+      if (!isCanceled) {
+        setIsScrolling(document.body.scrollHeight > document.body.clientHeight);
+      }
+    });
+    resizeObserver.observe(document.body);
+    return () => {
+      isCanceled = true;
+      resizeObserver.unobserve(document.body);
+    };
+  }, [sortedServiceNames]);
+
   // Handle isLoading & isError
 
   return (
     <>
-      <div className="flex flex-col gap-2 relative justify-items-center">
-        <div className={deploymentsStyles({ isEmpty })}>
-          <div className="flex flex-col min-w-0 gap-6 justify-start">
-            {sortedServiceNames?.map((serviceName, i) => (
-              <Service
-                key={serviceName}
-                serviceName={serviceName}
-                className={i % 2 === 1 ? 'md:hidden' : 'md:block'}
-              />
-            ))}
-          </div>
-          <RestateServer className={reactServerStyles({ isEmpty })}>
-            {isEmpty && <NoDeploymentPlaceholder />}
-            {size > 1 && <MultipleDeploymentsPlaceholder />}
-          </RestateServer>
-          <div className="flex flex-col min-w-0 gap-6 justify-start">
-            {sortedServiceNames?.map((serviceName, i) => (
-              <Service
-                key={serviceName}
-                serviceName={serviceName}
-                className={i % 2 === 0 ? 'hidden' : 'hidden md:block'}
-              />
-            ))}
-            {size === 1 && <OneDeploymentPlaceholder />}
-          </div>
-        </div>
-      </div>
-      <ServiceDetails />
-      <DeploymentDetails />
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 0: 1, 1024: 2 }}
+        className={deploymentsStyles({ isEmpty })}
+      >
+        <Masonry
+          gutter="1.5rem"
+          style={{ gap: 'calc(8rem + 150px)' }}
+          className={layoutStyles({ isScrolling })}
+        >
+          {sortedServiceNames?.map((serviceName, i) => (
+            <Service
+              key={serviceName}
+              serviceName={serviceName}
+              className="max-w-lg"
+            />
+          ))}
+          {size === 1 && <OneDeploymentPlaceholder />}
+        </Masonry>
+      </ResponsiveMasonry>
+      <RestateServer className={reactServerStyles({ isEmpty })}>
+        {isEmpty && <NoDeploymentPlaceholder />}
+      </RestateServer>
+      {size > 1 && <MultipleDeploymentsPlaceholder />}
     </>
   );
 }
