ddea95778a8516f48c5e50a1a1a81c2d2f3f58b1
Nik Nasr <nik@restate.dev>
2024-11-11 15:45:48 +0000

    add explainer
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/explainers/src/index.ts
+++ libs/features/explainers/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/ServiceDeployment';
 export * from './lib/Service';
 export * from './lib/ServiceType';
 export * from './lib/HandlerType';
+export * from './lib/ProtocolType';
+export * from './lib/ServiceCompatibility';
