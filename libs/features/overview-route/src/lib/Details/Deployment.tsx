b7913a8b2911bf1a8ed2ea645dfe677136986635
Nik Nasr <nik@restate.dev>
2024-11-11 18:05:53 +0000

    add autofocus
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/overview-route/src/lib/Details/Deployment.tsx
+++ libs/features/overview-route/src/lib/Details/Deployment.tsx
@@ -42,7 +42,11 @@ export function DeploymentDetails() {
           {error && <ErrorBanner errors={[error]} />}
           <div className="flex gap-2">
             <ComplementaryClose>
-              <Button className="flex-auto grow-0 w-1/2" variant="secondary">
+              <Button
+                autoFocus
+                className="flex-auto grow-0 w-1/2"
+                variant="secondary"
+              >
                 Cancel
               </Button>
             </ComplementaryClose>
