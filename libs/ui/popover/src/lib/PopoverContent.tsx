843f07b9898659d18a7ec95ded6d0f6e7595021b
Nik Nasr <nik@restate.dev>
2024-11-13 10:08:04 +0000

    add props
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/popover/src/lib/PopoverContent.tsx
+++ libs/ui/popover/src/lib/PopoverContent.tsx
@@ -1,14 +1,18 @@
 import { PropsWithChildren } from 'react';
 import { Dialog as AriaDialog } from 'react-aria-components';
 import { PopoverOverlay } from './PopoverOverlay';
+import { tv } from 'tailwind-variants';
 
+const styles = tv({
+  base: 'rounded-[1rem]',
+});
 export function PopoverContent({
   children,
   className,
   ...props
 }: PropsWithChildren<{ className?: string }>) {
   return (
-    <PopoverOverlay className="rounded-[1rem]">
+    <PopoverOverlay className={styles({ className })}>
       <AriaDialog className="outline bg-gray-100 rounded-[1rem] outline-0 overflow-auto relative">
         {children}
       </AriaDialog>
