8a6da42223ceb261c158f52c0465d4a6558e10a2
Nik Nasr <nik@restate.dev>
2024-11-13 10:19:53 +0000

    add export
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/overview-route/src/index.ts
+++ libs/features/overview-route/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/overview.route';
 export * from './lib/Details/Deployment';
 export * from './lib/Details/Service';
 export * from './lib/DeleteDeployment';
+export * from './lib/ServicePlayground';
