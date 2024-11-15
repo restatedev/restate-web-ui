2000014daa2df19ba0a81f714eff305b111c7ed5
Nik Nasr <nik@restate.dev>
2024-11-15 12:50:44 +0000

    add css style
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/layout/src/lib/ComplementaryOutlet.tsx
+++ libs/ui/layout/src/lib/ComplementaryOutlet.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'react';
+import styles from './Complementary.module.css';
 
 interface ComplementaryProps {
   id?: string;
@@ -9,7 +10,9 @@ export function ComplementaryOutlet(
 ) {
   return (
     <>
-      <div className="sm:pointer-events-none [background-image:none] 3xl:[background-image:none] sm:bg-gradient-to-l from-zinc-800/20 to-transparent to-50% [&:not(:has(+aside_[data-complementary-content]>*))]:hidden fixed z-50 sm:z-30 inset-0 bg-gray-800 sm:bg-transparent bg-opacity-30 transition-opacity" />
+      <div
+        className={`${styles.overlay} sm:pointer-events-none [background-image:none] 3xl:[background-image:none] sm:bg-gradient-to-l from-zinc-800/20 to-transparent to-50% fixed z-50 sm:z-30 inset-0 bg-gray-800 sm:bg-transparent bg-opacity-30 transition-opacity`}
+      />
       <aside className="[&:has(>*>*)]:duration-250 [&:has(>*>*)]:animate-in [&:has(>*>*)]:slide-in-from-right [&:has(>*>*)]:fade-in [&:not(has(>*>*))]:duration-250 flex flex-col 3xl:sticky top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 sm:translate-y-0 sm:translate-x-0 sm:top-24 3xl:top-[calc(0.75rem+3.5rem+2.5rem)] 3xl:px-0 3xl:pt-8 [&:not(:has([data-complementary-content]>*))]:hidden fixed z-50 lg:right-8 sm:right-6 right-auto sm:left-auto lg:bottom-6 sm:bottom-6 max-h-[90vh] max-w-[100vw] 3xl:max-h-auto sm:max-h-none lg:max-h-none 3xl:max-h-none">
         <div
           {...props}
