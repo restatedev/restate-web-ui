095116fcfdd2789ebc9e98067672aba12d81c274
Nik Nasr <nik@restate.dev>
2024-11-18 10:54:29 +0000

    add lib
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ libs/data-access/middleware-service-worker/README.md
@@ -0,0 +1,3 @@
+# middleware-service-worker
+
+This library was generated with [Nx](https://nx.dev).
---
+++ libs/data-access/middleware-service-worker/src/lib/middleware-service-worker.ts
@@ -0,0 +1,3 @@
+export function middlewareServiceWorker(): string {
+  return 'middleware-service-worker';
+}
