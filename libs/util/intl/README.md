a7795a7930842fbe73e51b39ef9ec8770afbd7e2
Nik Nasr <nik@restate.dev>
2024-12-04 18:31:15 +0100

    add intl
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ libs/util/intl/README.md
@@ -0,0 +1,7 @@
+# intl
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test intl` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/util/intl/src/lib/Intl.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledIntl = styled.div`
+  color: pink;
+`;
+
+export function Intl() {
+  return (
+    <StyledIntl>
+      <h1>Welcome to Intl!</h1>
+    </StyledIntl>
+  );
+}
+
+export default Intl;
