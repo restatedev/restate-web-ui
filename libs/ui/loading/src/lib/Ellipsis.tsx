ff55ba33d9238cf8622cb22c0a734ac341661ac7
Nik Nasr <nik@restate.dev>
2024-12-05 09:41:38 +0100

    add ellipsis
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ libs/ui/loading/src/lib/Ellipsis.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from 'react';
+import { tv } from 'tailwind-variants';
+
+const styles = tv({
+  base: 'inline-flex items-baseline gap-[0.15em]',
+  slots: {
+    ellipsis: 'inline-flex gap-[0.15em] items-baseline gap-[0.15em]',
+  },
+});
+
+export const Ellipsis = ({
+  className,
+  children,
+}: PropsWithChildren<{ className?: string }>) => {
+  const { base, ellipsis } = styles({ className });
+  return (
+    <div className={base()}>
+      {children}
+      <div className={ellipsis()}>
+        <div className="w-[0.15em] aspect-square bg-current [animation-duration:2000ms] rounded-full animate-ping" />
+        <div className="w-[0.15em] aspect-square bg-current [animation-delay:750ms]  [animation-duration:2000ms] rounded-full animate-ping" />
+        <div className="w-[0.15em] aspect-square bg-current [animation-delay:1250ms]  [animation-duration:2000ms] rounded-full animate-ping" />
+      </div>
+    </div>
+  );
+};
