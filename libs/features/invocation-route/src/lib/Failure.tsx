4edfb9f318587b7f6968ddc85c7c3941717e28de
Nik Nasr <nik@restate.dev>
2024-12-18 18:04:49 +0000

    add failure
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ libs/features/invocation-route/src/lib/Failure.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@restate/ui/button';
+import { DropdownSection } from '@restate/ui/dropdown';
+import { ErrorBanner } from '@restate/ui/error';
+import { Icon, IconName } from '@restate/ui/icons';
+import { Popover, PopoverContent, PopoverTrigger } from '@restate/ui/popover';
+import { RestateError } from '@restate/util/errors';
+import { useMemo } from 'react';
+import { tv } from 'tailwind-variants';
+const failureStyle = tv({
+  base: '',
+  slots: {
+    trigger:
+      'text-red-500 truncate bg-white/70 border px-1.5 py-0.5 flex rounded-md items-center gap-1 text-2xs h-5',
+    errorIcon: 'h-3 w-3 shrink-0 text-red-500/90',
+  },
+});
+export function Failure({
+  message,
+  restate_code,
+}: {
+  restate_code?: string;
+  message: string;
+}) {
+  const error = useMemo(
+    () => new RestateError(message, restate_code),
+    [message, restate_code]
+  );
+  const { trigger, errorIcon } = failureStyle();
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button variant="secondary" className={trigger()} disabled={!error}>
+          <Icon name={IconName.CircleX} className={errorIcon()} />
+          {restate_code}
+          <Icon
+            name={IconName.ChevronsUpDown}
+            className="h-3 w-3 text-gray-500 shrink-0"
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="max-w-lg">
+        <DropdownSection title="Failure">
+          <ErrorBanner
+            error={error}
+            className="rounded-lg [&_details]:max-h-[min(10rem,30vh)]"
+          />
+        </DropdownSection>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- libs/features/invocation-route/src/lib/entries/GetState.tsx
+++ libs/features/invocation-route/src/lib/entries/GetState.tsx
@@ -2,6 +2,7 @@ import { GetStateJournalEntryType } from '@restate/data-access/admin-api';
 import { EntryProps } from './types';
 import { Expression, InputOutput } from '../Expression';
 import { Value } from '../Value';
+import { Failure } from '../Failure';
 
 export function GetState({
   entry,
@@ -13,19 +14,28 @@ export function GetState({
       isFunction={false}
       name={entry.key ?? ''}
       output={
-        typeof entry.value === 'string' ? (
-          <InputOutput
-            name={entry.value}
-            popoverTitle="Value"
-            popoverContent={
-              <Value value={entry.value} className="text-xs font-mono py-3" />
-            }
-          />
-        ) : (
-          <div className="text-zinc-400 font-semibold font-mono text-2xs">
-            void
-          </div>
-        )
+        <>
+          {typeof entry.value === 'string' && (
+            <InputOutput
+              name={entry.value}
+              popoverTitle="Value"
+              popoverContent={
+                <Value value={entry.value} className="text-xs font-mono py-3" />
+              }
+            />
+          )}
+          {typeof entry.value === 'undefined' && !entry.failure && (
+            <div className="text-zinc-400 font-semibold font-mono text-2xs">
+              void
+            </div>
+          )}
+          {entry.failure?.message && (
+            <Failure
+              message={entry.failure.message}
+              restate_code={entry.failure.restate_code}
+            />
+          )}
+        </>
       }
     />
   );
