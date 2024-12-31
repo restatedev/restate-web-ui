c0f1a12e427146701899700326088b63bc43b944
Nik Nasr <nik@restate.dev>
2024-12-31 13:44:02 +0000

    lazy load prism
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/api/src/lib/API.tsx
+++ libs/ui/api/src/lib/API.tsx
@@ -1,18 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { lazy, useId } from 'react';
-import { Prism } from 'prism-react-renderer';
 
 if (typeof window !== 'undefined') {
-  (window as any).Prism = Prism;
+  import('prism-react-renderer');
   import('@stoplight/json-schema-viewer');
 }
 
-const JsonSchemaViewerInner = lazy(() =>
-  import('@stoplight/json-schema-viewer').then((module) => ({
-    default: module.JsonSchemaViewer,
-  }))
-);
+const JsonSchemaViewerInner = lazy(() => {
+  return import('prism-react-renderer').then((module) => {
+    (window as any).Prism = module.Prism;
+
+    return import('@stoplight/json-schema-viewer').then((module) => ({
+      default: module.JsonSchemaViewer,
+    }));
+  });
+});
 
 export const API = ({
   apiDescriptionDocument,
