a0d4bbc178d6217a92b50e4ed5ef43446945bf53
Nik Nasr <nik@restate.dev>
2025-01-16 14:29:30 +0000

    update titles
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- apps/web-ui/app/routes/invocations.tsx
+++ apps/web-ui/app/routes/invocations.tsx
@@ -2,3 +2,6 @@ import { invocations } from '@restate/features/invocations-route';
 
 export default invocations.Component;
 export const clientLoader = invocations.clientLoader;
+export function meta() {
+  return [{ title: 'Restate - Invocations' }];
+}
--- apps/web-ui/app/routes/overview.tsx
+++ apps/web-ui/app/routes/overview.tsx
@@ -1,3 +1,6 @@
 import { overview } from '@restate/features/overview-route';
 
+export function meta() {
+  return [{ title: 'Restate - Overview' }];
+}
 export default overview.Component;
