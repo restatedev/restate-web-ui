2fa418241df1dc0a53e20d37b544e09a8efd5e88
Nik Nasr <nik@restate.dev>
2025-01-27 11:42:38 +0000

    add routes
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- apps/web-ui/app/routes.ts
+++ apps/web-ui/app/routes.ts
@@ -1,7 +1,16 @@
-import { type RouteConfig, route, index } from '@react-router/dev/routes';
+import {
+  type RouteConfig,
+  route,
+  index,
+  prefix,
+} from '@react-router/dev/routes';
 
 export default [
   index('routes/_index.tsx'),
   route('overview', 'routes/overview.tsx'),
   route('invocations', 'routes/invocations.tsx'),
+  ...prefix('state', [
+    index('routes/state.tsx'),
+    route(':virtualObject', 'routes/virtual-object.tsx'),
+  ]),
 ] satisfies RouteConfig;
