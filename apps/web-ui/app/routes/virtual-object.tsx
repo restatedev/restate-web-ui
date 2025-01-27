ef3513148b3a445389120bad668b64dab7b79ba5
Nik Nasr <nik@restate.dev>
2025-01-27 11:46:13 +0000

    update title
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- apps/web-ui/app/routes/virtual-object.tsx
+++ apps/web-ui/app/routes/virtual-object.tsx
@@ -2,6 +2,6 @@ import { virtualObject } from '@restate/features/state-object-route';
 import { MetaFunction } from 'react-router';
 
 export const meta: MetaFunction = (args) => {
-  return [{ title: `Restate - Virtual Object - ${args.params.virtualObject}` }];
+  return [{ title: `Restate - State - ${args.params.virtualObject}` }];
 };
 export default virtualObject.Component;
