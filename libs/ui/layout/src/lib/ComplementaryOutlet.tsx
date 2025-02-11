16cc08ad476c406484f63e0171142dcfb7559b7d
Nik Nasr <nik@restate.dev>
2025-02-11 17:14:41 +0000

    fix scroll jumping
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/layout/src/lib/ComplementaryOutlet.tsx
+++ libs/ui/layout/src/lib/ComplementaryOutlet.tsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 import styles from './Complementary.module.css';
 
 interface ComplementaryProps {
@@ -13,10 +13,7 @@ export function ComplementaryOutlet(
       <div
         className={`${styles.overlay} sm:pointer-events-none [background-image:none] 3xl:[background-image:none] sm:bg-gradient-to-l from-zinc-800/20 to-transparent to-50% fixed z-[100] sm:z-30 inset-0 bg-gray-800 sm:bg-transparent bg-opacity-30 transition-opacity`}
       />
-      <aside
-        className="[&:has(>*>*)]:duration-250 [&:has(>*>*)]:animate-in [&:has(>*>*)]:slide-in-from-right [&:has(>*>*)]:fade-in [&:not(has(>*>*))]:duration-250 flex flex-col top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 sm:translate-y-0 sm:translate-x-0 sm:top-24 3xl:top-[calc(0.75rem+3.5rem+2.5rem)] 3xl:pt-10 [&:not(:has([data-complementary-content]>*))]:hidden fixed z-[100] sm:z-50 lg:right-8 sm:right-6 right-auto sm:left-auto lg:bottom-6 sm:bottom-6 max-h-[90vh] max-w-[100vw] sm:max-h-none lg:max-h-none 3xl:h2-fit 3xl:max-h-[1400px] 3xl:right-[calc(50vw-min(50rem,calc(50vw-400px-2rem))-400px)]
-]"
-      >
+      <aside className="transition translate-x-[calc(-50%-50vw)] left-[100vw] sm:translate-x-[calc(-100%-1.5rem)] lg:translate-x-[calc(-100%-2rem)] 3xl:translate-x-[calc(-50vw+min(50rem,calc(50vw-400px-2rem)))] [&:has(>*>*)]:duration-250 [&:has(>*>*)]:animate-in [&:has(>*>*)]:slide-in-from-right [&:has(>*>*)]:fade-in [&:not(has(>*>*))]:duration-250 flex flex-col top-[50%] -translate-y-1/2 sm:translate-y-0  sm:top-24 3xl:top-[calc(0.75rem+3.5rem+2.5rem)] 3xl:pt-10 [&:not(:has([data-complementary-content]>*))]:hidden fixed z-[100] sm:z-50 lg:bottom-6 sm:bottom-6 max-h-[90vh] max-w-[100vw] sm:max-h-none lg:max-h-none 3xl:h2-fit 3xl:max-h-[1400px] ">
         <div
           {...props}
           className="relative [&>*]:row-start-1 [&>*]:row-end-2 [&>*]:col-start-1 [&>*]:col-end-2 [&>[data-top=false]]:absolute [&>[data-top=false]:has(~[data-top=false]>*:nth-child(2)>*:nth-child(2))]:-right-3 [&>[data-top=false]:has(~[data-top=false])]:top-3 [&>[data-top=false]:has(~[data-top=false])]:bottom-3 [&>*[data-top=false]]:-right-1.5 [&>[data-top=false]]:top-1.5 [&>[data-top=false]]:bottom-1.5 h-full flex-auto max-h-[inherit] grid [grid-template-columns:1fr] [grid-template-rows:1fr] max-w-[90vw] w-[400px]"
