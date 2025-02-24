d0bd799f7dc393e923ae55454999e9ab0023fbab
Nik Nasr <nik@restate.dev>
2025-02-24 12:44:55 +0000

    fix content-type formatting
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/api/src/lib/API.tsx
+++ libs/ui/api/src/lib/API.tsx
@@ -1,21 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { lazy, useId } from 'react';
-
-if (typeof window !== 'undefined') {
-  import('prism-react-renderer');
-  import('@stoplight/json-schema-viewer');
-}
-
-const JsonSchemaViewerInner = lazy(() => {
-  return import('prism-react-renderer').then((module) => {
-    (window as any).Prism = module.Prism;
-
-    return import('@stoplight/json-schema-viewer').then((module) => ({
-      default: module.JsonSchemaViewer,
-    }));
-  });
-});
+import { useId, Component } from 'react';
+import type { ErrorInfo, PropsWithChildren } from 'react';
+import { Icon, IconName } from '@restate/ui/icons';
+import { JsonSchemaViewer as JsonSchemaViewerInner } from '@stoplight/json-schema-viewer';
 
 export const API = ({
   apiDescriptionDocument,
@@ -44,11 +32,48 @@ export const JsonSchemaViewer = ({
   className?: string;
 }) => {
   return (
-    <JsonSchemaViewerInner
-      className={className}
-      schema={schema as any}
-      disableCrumbs
-      renderRootTreeLines
-    />
+    <ErrorBoundary>
+      <JsonSchemaViewerInner
+        className={className}
+        schema={schema as any}
+        disableCrumbs
+        renderRootTreeLines
+      />
+    </ErrorBoundary>
   );
 };
+
+class ErrorBoundary extends Component<
+  PropsWithChildren,
+  {
+    hasError: boolean;
+  }
+> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): {
+    hasError: boolean;
+  } {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Error caught by ErrorBoundary: ', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="truncate max-w-full flex items-center text-red-500 gap-2 flex-wrap w-full min-w-0 my-1  px-2  py-1 text-code rounded-lg ">
+          <Icon name={IconName.TriangleAlert} className="w-3.5 h-3.5" /> Failed
+          to display the content type
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
