9cf5891649926a8b9e48d27c7c2c1f6562b0fac3
Nik Nasr <nik@restate.dev>
2025-03-17 12:33:44 +0000

    remove convert to utc
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/data-access/query/src/lib/convertInvocation.ts
+++ libs/data-access/query/src/lib/convertInvocation.ts
@@ -1,17 +1,9 @@
 import type { RawInvocation } from '@restate/data-access/admin-api/spec';
 import { getComputedInvocationStatus } from './getComputedInvocationStatus';
-import { convertToUTC } from './convertToUTC';
 
 export function convertInvocation(invocation: RawInvocation) {
   return {
     ...invocation,
     status: getComputedInvocationStatus(invocation),
-    last_start_at: convertToUTC(invocation.last_start_at),
-    running_at: convertToUTC(invocation.running_at),
-    modified_at: convertToUTC(invocation.modified_at),
-    inboxed_at: convertToUTC(invocation.inboxed_at),
-    scheduled_at: convertToUTC(invocation.scheduled_at),
-    completed_at: convertToUTC(invocation.completed_at),
-    created_at: convertToUTC(invocation.created_at),
   };
 }
