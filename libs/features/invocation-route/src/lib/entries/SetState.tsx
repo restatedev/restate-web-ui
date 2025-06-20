49d46260c038f46779b18bfd09bd7ec79f4f806d
Nik Nasr <nik@restate.dev>
2025-06-20 17:16:39 +0100

    update set state
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-route/src/lib/entries/SetState.tsx
+++ libs/features/invocation-route/src/lib/entries/SetState.tsx
@@ -1,6 +1,8 @@
 import { JournalEntryV2 } from '@restate/data-access/admin-api';
 import { EntryProps } from './types';
 import { EntryExpression } from './EntryExpression';
+import { InputOutput } from '../Expression';
+import { Value } from '../Value';
 
 export function SetState({
   entry,
@@ -21,6 +23,29 @@ export function SetState({
         },
         { paramName: 'value', title: 'Value', placeholderLabel: 'value' },
       ]}
+      input={
+        <>
+          <InputOutput
+            name={JSON.stringify(entry.key)}
+            popoverTitle="Key"
+            popoverContent={
+              <Value value={entry.key} className="text-xs font-mono py-3" />
+            }
+          />
+          <div>,</div>
+          <InputOutput
+            name="value"
+            popoverTitle="Value"
+            isValueHidden
+            popoverContent={
+              <Value
+                value={entry.value}
+                className="text-xs font-mono py-3 mx-0.5"
+              />
+            }
+          />
+        </>
+      }
       operationSymbol=""
     />
   );
