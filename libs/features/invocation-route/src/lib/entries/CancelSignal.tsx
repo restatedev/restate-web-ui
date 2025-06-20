7ab07e0567e0468d54f6ba4e930c4283b93dcb1e
Nik Nasr <nik@restate.dev>
2025-06-20 11:17:21 +0100

    update cancel
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-route/src/lib/entries/CancelSignal.tsx
+++ libs/features/invocation-route/src/lib/entries/CancelSignal.tsx
@@ -1,18 +1,19 @@
 import { JournalEntryV2 } from '@restate/data-access/admin-api';
 import { EntryProps } from './types';
+import { EntryExpression } from './EntryExpression';
 
 export function CancelSignal({
   entry,
-  failed,
   invocation,
-  error,
-  isRetrying,
-  wasRetrying,
 }: EntryProps<
   Extract<JournalEntryV2, { type?: 'Cancel'; category?: 'notification' }>
 >) {
   return (
-    <div className="text-zinc-600">
+    <EntryExpression entry={entry} invocation={invocation} operationSymbol="" />
+  );
+
+  return (
+    <div className="text-zinc-600 font-sans">
       <span className="font-medium font italic text-blue-500">
         Cancellation
       </span>{' '}
