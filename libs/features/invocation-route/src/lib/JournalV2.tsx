182883116d32293d2bc74b8ff2b72d713d96f6df
Nik Nasr <nik@restate.dev>
2025-06-23 12:58:53 +0100

    add journalv2
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-route/src/lib/JournalV2.tsx
+++ libs/features/invocation-route/src/lib/JournalV2.tsx
@@ -1,7 +1,9 @@
-import { useGetInvocationsJournalWithInvocationsV2 } from '@restate/data-access/admin-api';
-import { lazy, Suspense, useRef, useState } from 'react';
+import {
+  JournalEntryV2,
+  useGetInvocationsJournalWithInvocationsV2,
+} from '@restate/data-access/admin-api';
+import { lazy, Suspense, useCallback, useState } from 'react';
 import { InvocationId } from './InvocationId';
-import { tv } from 'tailwind-variants';
 import { SnapshotTimeProvider } from '@restate/util/snapshot-time';
 import { Link } from '@restate/ui/link';
 import { Icon, IconName } from '@restate/ui/icons';
@@ -9,9 +11,12 @@ import { Button } from '@restate/ui/button';
 import { HoverTooltip } from '@restate/ui/tooltip';
 import { useRestateContext } from '@restate/features/restate-context';
 import { ErrorBanner } from '@restate/ui/error';
-import { Entries, getEntryId, getTimelineId, isEntryCall } from './Entries';
 import { JournalContextProvider } from './JournalContext';
 import { Spinner } from '@restate/ui/loading';
+import { Entry } from './Entry';
+import { Input } from './entries/Input';
+import { getTimelineId, PortalProvider, usePortals } from './Portals';
+import { LifeCycleProgress, Units } from './LifeCycleProgress';
 
 const LazyPanel = lazy(() =>
   import('react-resizable-panels').then((m) => ({ default: m.Panel }))
@@ -26,10 +31,6 @@ const LazyPanelResizeHandle = lazy(() =>
     default: m.PanelResizeHandle,
   }))
 );
-
-const styles = tv({
-  base: 'mt-8 gap-1',
-});
 
 export function JournalV2({
   invocationId,
@@ -49,18 +50,31 @@ export function JournalV2({
     isPending,
     error: apiError,
     refetch,
-    dataUpdatedAt,
+    invalidate,
+    dataUpdatedAt: allQueriesDataUpdatedAt,
   } = useGetInvocationsJournalWithInvocationsV2(invocationIds, {
     refetchOnMount: true,
     staleTime: 0,
   });
+
+  const addInvocationId = useCallback(
+    (id: string) => {
+      invalidate();
+      setInvocationIds((ids) => [...ids, id]);
+    },
+    [invalidate]
+  );
+  const removeInvocationId = useCallback((idToBeRemoved: string) => {
+    setInvocationIds((ids) => {
+      return ids.filter((id) => id !== idToBeRemoved);
+    });
+  }, []);
 
   const journalAndInvocationData = data?.[invocationId];
 
   const { baseUrl } = useRestateContext();
 
   const combinedEntries = getCombinedJournal(invocationId, data);
-  const containerRef = useRef<HTMLDivElement>(null);
 
   const invocationApiError = apiError?.[invocationId];
 
@@ -68,34 +82,36 @@ export function JournalV2({
     return <ErrorBanner error={invocationApiError} />;
   }
 
+  const dataUpdatedAt = allQueriesDataUpdatedAt[invocationId]!;
+
+  if (!journalAndInvocationData) {
+    return null;
+  }
   const start = new Date(
     journalAndInvocationData?.created_at ??
       new Date(dataUpdatedAt).toISOString()
   ).getTime();
-  const end = new Date(
-    journalAndInvocationData?.completed_at ??
-      new Date(dataUpdatedAt).toISOString()
-  ).getTime();
-
-  const cancelEntries = journalAndInvocationData?.journal?.entries?.filter(
-    (entry) => entry.type === 'Cancel' && entry.category === 'notification'
+  const end = Math.max(
+    ...(journalAndInvocationData.journal?.entries?.map((entry) =>
+      entry.end
+        ? new Date(entry.end).getTime()
+        : entry.start
+        ? new Date(entry.start).getTime()
+        : -1
+    ) ?? []),
+    !journalAndInvocationData.completed_at ? dataUpdatedAt : -1
   );
 
-  const isOldJournal =
-    !journalAndInvocationData?.journal?.version ||
-    journalAndInvocationData?.journal?.version === 1;
-
   return (
-    <div ref={containerRef}>
+    <PortalProvider>
       <JournalContextProvider
         invocationIds={invocationIds}
-        setInvocationIds={setInvocationIds}
+        addInvocationId={addInvocationId}
+        removeInvocationId={removeInvocationId}
         start={start}
         end={end}
         dataUpdatedAt={dataUpdatedAt}
-        cancelTime={cancelEntries?.at(0)?.start}
         isPending={isPending}
-        containerRef={containerRef}
         error={apiError}
       >
         <SnapshotTimeProvider lastSnapshot={dataUpdatedAt}>
@@ -107,116 +123,165 @@ export function JournalV2({
               </div>
             }
           >
-            <LazyPanelGroup
-              direction="horizontal"
-              className={styles({ className })}
-            >
-              <LazyPanel
-                defaultSize={(1 - timelineWidth) * 100}
-                className="pb-4"
-              >
-                <div className="flex  flex-col items-start gap-1.5">
-                  <div className="flex items-center gap-1.5 w-full h-7 relative">
-                    <div className="w-2 bg-zinc-300 absolute left-2.5 h-2 rounded-full">
-                      <div className="w-px absolute  left-1/2 h-4 top-full -translate-x-1/2 border-zinc-300 border border-dashed" />
-                    </div>
-                    <div className="shrink-0 text-xs uppercase font-semibold text-gray-400 pl-6">
-                      Invoked by
-                    </div>
-                    {journalAndInvocationData?.invoked_by === 'ingress' ? (
-                      <div className="text-xs font-medium">Ingress</div>
-                    ) : journalAndInvocationData?.invoked_by_id ? (
-                      <InvocationId
-                        id={journalAndInvocationData?.invoked_by_id}
-                        className="min-w-0 max-w-[20ch] font-medium"
-                        size="sm"
-                      />
-                    ) : null}
-                  </div>
-                  <Entries invocationId={invocationId} showInputEntry />
-                  <div
-                    className="grid grid-cols-1 text-xs items-start gap-1.5 pl-2 w-full"
-                    data-container
-                  >
-                    {combinedEntries?.map((entry, index) => (
-                      <div
-                        data-id={getEntryId(
-                          entry.invocationId,
-                          entry.entryIndex
-                        )}
-                        className="contents [&:has(*)]:block"
-                        key={index}
-                        style={{ paddingLeft: `${entry.depth * 1.125}rem` }}
-                      />
-                    ))}
-                  </div>
+            <div className="flex h-9 items-center absolute -top-9 w-full">
+              <div className="flex items-center gap-1.5 w-full h-full relative">
+                <div className="w-2 bg-zinc-300 absolute left-2.5 h-2 rounded-full">
+                  <div className="w-px absolute  left-1/2 h-3.5 top-full -translate-x-1/2  border-zinc-300 border border-dashed" />
                 </div>
-              </LazyPanel>
-              {timelineWidth !== 0 && (
-                <LazyPanelResizeHandle className="w-2 hidden md:flex justify-center items-center group relative">
-                  <div className="w-px group-hover:w-[2px] absolute left-1/2 top-8 bottom-8 bg-gray-200  group-hover:bg-blue-500" />
-                </LazyPanelResizeHandle>
-              )}
-              {
+                <div className="shrink-0 text-xs uppercase font-semibold text-gray-400 pl-6">
+                  Invoked by
+                </div>
+                {journalAndInvocationData?.invoked_by === 'ingress' ? (
+                  <div className="text-xs font-medium">Ingress</div>
+                ) : journalAndInvocationData?.invoked_by_id ? (
+                  <InvocationId
+                    id={journalAndInvocationData?.invoked_by_id}
+                    className="min-w-0 max-w-[20ch] font-semibold text-code"
+                  />
+                ) : null}
+              </div>
+              <div className="ml-auto flex flex-row gap-2 items-center justify-end ">
+                <HoverTooltip content="Refresh">
+                  <Button variant="icon" onClick={refetch}>
+                    <Icon name={IconName.Retry} className="w-4 h-4" />
+                  </Button>
+                </HoverTooltip>
+                <HoverTooltip content="Introspect">
+                  <Link
+                    variant="icon"
+                    href={`${baseUrl}/introspection?query=SELECT id, index, appended_at, entry_type, name, entry_lite_json AS metadata FROM sys_journal WHERE id = '${journalAndInvocationData?.id}'`}
+                    target="_blank"
+                  >
+                    <Icon name={IconName.ScanSearch} className="w-4 h-4" />
+                  </Link>
+                </HoverTooltip>
+              </div>
+            </div>
+            <div className="relative text-code font-mono">
+              <LazyPanelGroup
+                direction="horizontal"
+                className={'gap-0'}
+                style={{ overflow: 'visible' }}
+              >
                 <LazyPanel
-                  defaultSize={timelineWidth * 100}
-                  className="hidden md:flex"
+                  defaultSize={(1 - timelineWidth) * 100}
+                  className="z-10 "
                 >
-                  <div className="w-full h-full overflow-auto flex flex-col gap-0">
-                    <div className="h-7 flex flex-row gap-2 items-center justify-end mb-[0.375rem]">
-                      <HoverTooltip content="Refresh">
-                        <Button variant="icon" onClick={refetch}>
-                          <Icon name={IconName.Retry} className="w-4 h-4" />
-                        </Button>
-                      </HoverTooltip>
-                      <HoverTooltip content="Introspect">
-                        <Link
-                          variant="icon"
-                          href={`${baseUrl}/introspection?query=SELECT id, index, appended_at, entry_type, name, entry_lite_json AS metadata FROM sys_journal WHERE id = '${journalAndInvocationData?.id}'`}
-                          target="_blank"
-                        >
-                          <Icon
-                            name={IconName.ScanSearch}
-                            className="w-4 h-4"
-                          />
-                        </Link>
-                      </HoverTooltip>
+                  <div className="border-0 border-white/50 border-r-0  bg-gradient-to-b from-gray-50 to-white shadow-sm rounded-2xl rounded-r-none relative overflow-hidden ">
+                    <div className="ring-1 ring-black/5 shadow-sm z-10 h-12 box-border border-b border-transparent last:border-none flex items-center bg-gray-100 rounded-bl-2xl">
+                      <Input
+                        entry={
+                          combinedEntries?.find(
+                            (combinedEntry) =>
+                              combinedEntry.invocationId === invocationId &&
+                              combinedEntry.entry?.type === 'Input'
+                          )?.entry as Extract<
+                            JournalEntryV2,
+                            { type?: 'Input'; category?: 'command' }
+                          >
+                        }
+                        invocation={data?.[invocationId]}
+                        className="w-full "
+                      />
                     </div>
+                    {combinedEntries?.map(
+                      ({ invocationId, entry, depth }, index) => {
+                        const invocation = data?.[invocationId];
+                        if (entry?.type === 'Input') {
+                          return null;
+                        }
 
-                    {!isOldJournal &&
-                      timelineWidth !== 0 &&
-                      combinedEntries?.map((entry, index) => (
-                        <div
-                          data-id={getTimelineId(
-                            entry.invocationId,
-                            entry.entryIndex
-                          )}
-                          className="[&:has(*)]:min-h-7 [&:has(*)]:mb-1.5 [&:nth-child(2)]:min-h-[1.875rem]"
-                          key={index}
-                        />
-                      ))}
-                    {isOldJournal && timelineWidth !== 0 && (
-                      <div className="text-code mb-4 p-2 py-4 text-zinc-500 text-center rounded-xl border bg-gray-200/50 shadow-[inset_0_1px_0px_0px_rgba(0,0,0,0.03)]">
-                        Update to the latest SDK to see more details in your
-                        journal. Check the{' '}
-                        <Link
-                          href="https://docs.restate.dev/operate/versioning#deploying-new-service-versions"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          docs
-                        </Link>{' '}
-                        for more info.
-                      </div>
+                        return (
+                          <Entry
+                            invocation={invocation}
+                            entry={entry}
+                            depth={depth}
+                            key={
+                              invocationId +
+                              entry?.category +
+                              entry?.type +
+                              index
+                            }
+                          />
+                        );
+                      }
                     )}
                   </div>
                 </LazyPanel>
-              }
-            </LazyPanelGroup>
+                <LazyPanelResizeHandle className="w-px hidden md:flex justify-center items-center group relative">
+                  <div className="w-px group-hover:w-[2px] absolute left-0 top-3 bottom-3 bg-transparent group-hover:bg-blue-500" />
+                </LazyPanelResizeHandle>
+                <LazyPanel
+                  defaultSize={timelineWidth * 100}
+                  className="hidden md:block relative"
+                  style={{ overflow: 'visible' }}
+                >
+                  <Units
+                    className=" absolute inset-0"
+                    invocation={journalAndInvocationData}
+                  />
+                  <div className=" border border-transparent">
+                    <LifeCycleProgress
+                      className=" h-12 px-2"
+                      invocation={journalAndInvocationData}
+                    />
+                  </div>
+                  {combinedEntries
+                    ?.filter(({ entry }) => entry?.type !== 'Input')
+                    .map(({ entry, invocationId }) => (
+                      <TimelineContainer
+                        invocationId={invocationId}
+                        entry={entry}
+                        key={getTimelineId(
+                          invocationId,
+                          entry?.index,
+                          entry?.type,
+                          entry?.category
+                        )}
+                      />
+                    ))}
+                </LazyPanel>
+              </LazyPanelGroup>
+            </div>
           </Suspense>
         </SnapshotTimeProvider>
       </JournalContextProvider>
+    </PortalProvider>
+  );
+}
+
+function TimelineContainer({
+  invocationId,
+  entry,
+}: {
+  invocationId: string;
+  entry?: JournalEntryV2;
+}) {
+  const { setPortal } = usePortals(
+    getTimelineId(invocationId, entry?.index, entry?.type, entry?.category)
+  );
+  return (
+    <div
+      className="w-full [content-visibility:auto] pl-2 pr-2 h-9 border-b  border-transparent relative [&:not(:has(>*+*))]:hidden"
+      ref={setPortal}
+    >
+      <div className="absolute left-0 right-0 h-px top-1/2 border-gray-300/70  translate-y-[-1px] border-dashed border-b border-spacing-10" />
     </div>
+  );
+}
+
+function isExpandable(
+  entry: JournalEntryV2
+): entry is
+  | Extract<JournalEntryV2, { type?: 'Call'; category?: 'command' }>
+  | Extract<
+      JournalEntryV2,
+      { type?: 'AttachInvocation'; category?: 'command' }
+    > {
+  return Boolean(
+    entry.type &&
+      ['Call', 'AttachInvocation'].includes(entry.type) &&
+      entry.category === 'command'
   );
 }
 
@@ -227,7 +292,7 @@ function getCombinedJournal(
 ):
   | {
       invocationId: string;
-      entryIndex: number;
+      entry?: JournalEntryV2;
       depth: number;
     }[]
   | undefined {
@@ -235,21 +300,19 @@ function getCombinedJournal(
     return [
       {
         invocationId,
-        entryIndex: 0,
         depth,
       },
     ];
   }
 
-  const length = (data?.[invocationId]?.journal?.entries ?? []).length;
   const combinedEntries = data?.[invocationId]?.journal?.entries
     ?.map((entry) => {
-      if (isEntryCall(entry)) {
+      if (isExpandable(entry)) {
         const callInvocationId = String(entry.invocationId);
         return [
           {
             invocationId,
-            entryIndex: entry.index ?? -1,
+            entry,
             depth,
           },
           ...(getCombinedJournal(callInvocationId, data, depth + 1)?.flat() ??
@@ -258,19 +321,12 @@ function getCombinedJournal(
       } else {
         return {
           invocationId,
-          entryIndex: entry.index ?? -1,
+          entry,
           depth,
         };
       }
     })
     .flat();
 
-  return [
-    ...(combinedEntries ?? []),
-    {
-      invocationId,
-      entryIndex: length,
-      depth,
-    },
-  ];
+  return combinedEntries;
 }
