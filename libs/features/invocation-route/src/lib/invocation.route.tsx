06aaac5f8a4198fc441861951fe360742554a40e
Nik Nasr <nik@restate.dev>
2025-06-26 16:45:59 +0100

    update pending animation
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-route/src/lib/invocation.route.tsx
+++ libs/features/invocation-route/src/lib/invocation.route.tsx
@@ -17,7 +17,7 @@ import { WorkflowKeySection } from './WorkflowKeySection';
 import { tv } from 'tailwind-variants';
 
 const metadataContainerStyles = tv({
-  base: 'mt-6 mb-24 rounded-xl grid grid-cols-1 md:grid-cols-2 gap-2 gap-y-4',
+  base: 'mt-6 rounded-xl grid-cols-1 md:grid-cols-2 gap-2 gap-y-4 [&:has(*)]:grid hidden',
   variants: {
     isVirtualObject: {
       true: 'lg:grid-cols-2 2xl:grid-cols-4',
@@ -26,6 +26,10 @@ const metadataContainerStyles = tv({
     isWorkflow: {
       true: 'lg:grid-cols-3',
       false: '',
+    },
+    isPending: {
+      true: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-4',
+      false: 'mb-24',
     },
   },
 });
@@ -93,45 +97,55 @@ function Component() {
           </div>
         </div>
 
-        {journalAndInvocationData && (
-          <div
-            className={metadataContainerStyles({ isVirtualObject, isWorkflow })}
-          >
-            <KeysIdsSection
-              invocation={journalAndInvocationData}
-              className="p-0 rounded-xl border bg-gray-200/50 h-fit  [&>*:last-child]:border-white/50 [&>*:last-child]:rounded-xl [&>*:last-child]:bg-gradient-to-b [&>*:last-child]:to-gray-50/80 [&>*:last-child]:from-gray-50  [&>*:last-child]:shadow-zinc-800/[0.03]"
-            />
-            <DeploymentSection
-              invocation={journalAndInvocationData}
-              className="p-0 rounded-xl border h-fit bg-gray-200/50  [&>*:last-child]:border-white/50 [&>*:last-child]:rounded-xl [&>*:last-child]:bg-gradient-to-b [&>*:last-child]:to-gray-50/80 [&>*:last-child]:from-gray-50  [&>*:last-child]:shadow-zinc-800/[0.03]"
-              raised
-            />
-            <VirtualObjectSection
-              invocation={journalAndInvocationData}
-              raised
-              className="contents [&>*:last-child>h3]:mt-0 [&>*]:rounded-xl  [&>*]:border [&>*]:h-fit [&>*]:bg-gray-200/50  [&>*>*:last-child]:rounded-xl  [&>*>*:last-child]:border-white/50 [&>*>*:last-child]:bg-gradient-to-b [&>*>*:last-child]:to-gray-50/80 [&>*>*:last-child]:from-gray-50  [&>*>*:last-child]:shadow-zinc-800/[0.03]"
-            />
-            <WorkflowKeySection
-              invocation={journalAndInvocationData}
-              raised
-              className="p-0 rounded-xl border h-fit bg-gray-200/50  [&>*:last-child]:border-white/50 [&>*:last-child]:rounded-xl [&>*:last-child]:bg-gradient-to-b [&>*:last-child]:to-gray-50/80 [&>*:last-child]:from-gray-50  [&>*:last-child]:shadow-zinc-800/[0.03]"
-            />
-            {shouldShowFailure && (
-              <Section className={lastFailureContainer()}>
-                <SectionTitle>
-                  {isFailed ? 'Completion failure' : 'Last failure'}
-                </SectionTitle>
-                <SectionContent className="flex-auto rounded-xl border-white/50 bg-gradient-to-b to-gray-50/80 from-gray-50 shadow-zinc-800/[0.03]">
-                  <ErrorBanner
-                    error={lastError}
-                    wrap={hasStack}
-                    className="bg-transparent p-0 [&_code]:bg-gray-200/50 h-full [&_details]:max-h-48"
-                  />
-                </SectionContent>
-              </Section>
-            )}
-          </div>
-        )}
+        <div
+          className={metadataContainerStyles({
+            isVirtualObject,
+            isPending,
+            isWorkflow,
+          })}
+        >
+          {isPending && (
+            <>
+              <div className="animate-pulse min-h-24 w-full rounded-xl bg-slate-200" />
+              <div className="animate-pulse min-h-24 w-full rounded-xl bg-slate-200" />
+              <div className="animate-pulse min-h-24 w-full rounded-xl bg-slate-200 lg:block hidden" />
+              <div className="animate-pulse min-h-24 w-full rounded-xl bg-slate-200 lg:block hidden" />
+            </>
+          )}
+          <KeysIdsSection
+            invocation={journalAndInvocationData}
+            className="p-0 rounded-xl border bg-gray-200/50 h-fit  [&>*:last-child]:border-white/50 [&>*:last-child]:rounded-xl [&>*:last-child]:bg-gradient-to-b [&>*:last-child]:to-gray-50/80 [&>*:last-child]:from-gray-50  [&>*:last-child]:shadow-zinc-800/[0.03]"
+          />
+          <DeploymentSection
+            invocation={journalAndInvocationData}
+            className="p-0 rounded-xl border h-fit bg-gray-200/50  [&>*:last-child]:border-white/50 [&>*:last-child]:rounded-xl [&>*:last-child]:bg-gradient-to-b [&>*:last-child]:to-gray-50/80 [&>*:last-child]:from-gray-50  [&>*:last-child]:shadow-zinc-800/[0.03]"
+            raised
+          />
+          <VirtualObjectSection
+            invocation={journalAndInvocationData}
+            raised
+            className="contents [&>*:last-child>h3]:mt-0 [&>*]:rounded-xl  [&>*]:border [&>*]:h-fit [&>*]:bg-gray-200/50  [&>*>*:last-child]:rounded-xl  [&>*>*:last-child]:border-white/50 [&>*>*:last-child]:bg-gradient-to-b [&>*>*:last-child]:to-gray-50/80 [&>*>*:last-child]:from-gray-50  [&>*>*:last-child]:shadow-zinc-800/[0.03]"
+          />
+          <WorkflowKeySection
+            invocation={journalAndInvocationData}
+            raised
+            className="p-0 rounded-xl border h-fit bg-gray-200/50  [&>*:last-child]:border-white/50 [&>*:last-child]:rounded-xl [&>*:last-child]:bg-gradient-to-b [&>*:last-child]:to-gray-50/80 [&>*:last-child]:from-gray-50  [&>*:last-child]:shadow-zinc-800/[0.03]"
+          />
+          {shouldShowFailure && (
+            <Section className={lastFailureContainer()}>
+              <SectionTitle>
+                {isFailed ? 'Completion failure' : 'Last failure'}
+              </SectionTitle>
+              <SectionContent className="flex-auto rounded-xl border-white/50 bg-gradient-to-b to-gray-50/80 from-gray-50 shadow-zinc-800/[0.03]">
+                <ErrorBanner
+                  error={lastError}
+                  wrap={hasStack}
+                  className="bg-transparent p-0 [&_code]:bg-gray-200/50 h-full [&_details]:max-h-48"
+                />
+              </SectionContent>
+            </Section>
+          )}
+        </div>
 
         <div className="flex flex-col mt-4">
           <div className="rounded-2xl border bg-gray-200/50 relative">
