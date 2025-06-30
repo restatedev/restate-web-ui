c66fbab57cddbbf66ce2c3ef9e5d825ab06419c9
Nik Nasr <nik@restate.dev>
2025-06-30 18:41:20 +0100

    refactor
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-route/src/lib/Value.tsx
+++ libs/features/invocation-route/src/lib/Value.tsx
@@ -1,18 +1,6 @@
-import { Icon, IconName } from '@restate/ui/icons';
-import { Spinner } from '@restate/ui/loading';
-import {
-  Component,
-  ErrorInfo,
-  lazy,
-  PropsWithChildren,
-  Suspense,
-  useRef,
-} from 'react';
+import { useRef } from 'react';
 import type { editor } from 'monaco-editor';
-
-const ValueMonaco = lazy(() =>
-  import('./.client/ValueMonaco').then((m) => ({ default: m.ValueMonaco }))
-);
+import { Editor } from '@restate/ui/editor';
 
 export function Value({
   value,
@@ -28,54 +16,11 @@ export function Value({
   }
 
   return (
-    <ErrorBoundary>
-      <div className={className}>
-        <Suspense
-          fallback={
-            <div className="flex items-center gap-1.5 text-sm text-zinc-500">
-              <Spinner className="w-4 h-4" />
-              Loading…
-            </div>
-          }
-        >
-          <ValueMonaco value={value} editorRef={editorRef} />
-        </Suspense>
-      </div>
-    </ErrorBoundary>
+    <Editor
+      value={value}
+      editorRef={editorRef}
+      readonly
+      className={className}
+    />
   );
-}
-
-class ErrorBoundary extends Component<
-  PropsWithChildren,
-  {
-    hasError: boolean;
-  }
-> {
-  constructor(props: PropsWithChildren) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(_: Error): {
-    hasError: boolean;
-  } {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Error caught by ErrorBoundary: ', error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div className="truncate max-w-full flex items-center text-red-500 gap-2 flex-wrap w-full min-w-0 my-1  px-2  py-1 text-code rounded-lg ">
-          <Icon name={IconName.TriangleAlert} className="w-3.5 h-3.5" /> Failed
-          to display value
-        </div>
-      );
-    }
-
-    return this.props.children;
-  }
 }
