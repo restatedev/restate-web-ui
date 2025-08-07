1c540a8b9291fc31cf137784fd9b547ba825bae4
Nik Nasr <nik@restate.dev>
2025-08-07 23:05:04 +0100

    fix tv
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/util/styles/src/lib/styles.ts
+++ libs/util/styles/src/lib/styles.ts
@@ -1,9 +1,1 @@
-import { createTV } from 'tailwind-variants';
-
-export const tv = createTV({
-  twMergeConfig: {
-    classGroups: {
-      'font-size': [{ text: ['code', '2xs', '3xs'] }],
-    },
-  },
-});
+export { tv } from 'tailwind-variants';
