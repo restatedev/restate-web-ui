f43063b584bb98c1d24f4e409494e2b88e103f86
Nik <nik@restate.dev>
2025-08-12 14:19:45 +0100

    QueryProvider can accepts custom QueryClient (#338)
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/util/react-query/src/lib/QueryProvider.tsx
+++ libs/util/react-query/src/lib/QueryProvider.tsx
@@ -1,11 +1,9 @@
-import { UnauthorizedError } from '@restate/util/errors';
 import {
   QueryClient,
   QueryClientProvider,
-  QueryCache,
   hydrate,
 } from '@tanstack/react-query';
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import { useMatches } from 'react-router';
 
 const useDehydratedState = (queryClient: QueryClient) => {
@@ -21,41 +19,10 @@ const useDehydratedState = (queryClient: QueryClient) => {
 
 export function QueryProvider({
   children,
-  logOut,
-  onError,
-  onSettled,
-  onSuccess,
-}: PropsWithChildren<
-  {
-    logOut?: (params: { persistRedirectUrl?: boolean }) => void;
-  } & NonNullable<ConstructorParameters<typeof QueryCache>[0]>
->) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        queryCache: new QueryCache({
-          onError: (error, query) => {
-            if (error instanceof UnauthorizedError) {
-              logOut?.({ persistRedirectUrl: true });
-            }
-            onError?.(error, query);
-          },
-          onSettled,
-          onSuccess,
-        }),
-        defaultOptions: {
-          queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 5 * 60 * 1000,
-            retry: false,
-            refetchOnMount: false,
-            experimental_prefetchInRender: true,
-          },
-        },
-      }),
-  );
-  // TODO
+  queryClient,
+}: PropsWithChildren<{
+  queryClient: QueryClient;
+}>) {
   useDehydratedState(queryClient);
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
