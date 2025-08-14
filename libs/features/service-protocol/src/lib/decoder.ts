d70178c8f47431faa137bc8666d70b465b98e96f
Nik Nasr <nik@restate.dev>
2025-08-14 17:42:47 +0100

    more decoding
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/service-protocol/src/lib/decoder.ts
+++ libs/features/service-protocol/src/lib/decoder.ts
@@ -16,7 +16,10 @@ export function base64ToUtf8(base64?: string) {
   return new TextDecoder().decode(bytes);
 }
 
-export function utf8ToBase64(str: string) {
+export function utf8ToBase64(str?: string) {
+  if (!str) {
+    return str;
+  }
   const bytes = new TextEncoder().encode(str); // UTF-8 → bytes
   let binary = '';
   bytes.forEach((b) => (binary += String.fromCharCode(b))); // bytes → binary string
@@ -25,4 +28,36 @@ export function utf8ToBase64(str: string) {
 
 export function decode(bytes: Uint8Array) {
   return bytesToBase64(bytes);
+}
+
+export function hexToBase64(hex?: string) {
+  if (!hex) {
+    return undefined;
+  }
+  if (typeof hex !== 'string' || hex.length % 2 !== 0) {
+    throw new Error('Invalid hex string');
+  }
+
+  const bytes = new Uint8Array(hex.length / 2);
+  for (let i = 0; i < hex.length; i += 2) {
+    bytes[i / 2] = parseInt(hex.substr(i, 2), 16);
+  }
+
+  let binary = '';
+  bytes.forEach((b) => (binary += String.fromCharCode(b)));
+
+  return btoa(binary);
+}
+
+export function base64ToHex(base64: string) {
+  // Decode Base64 → binary string
+  const binary = atob(base64);
+
+  // Binary string → hex
+  let hex = '';
+  for (let i = 0; i < binary.length; i++) {
+    hex += binary.charCodeAt(i).toString(16).padStart(2, '0');
+  }
+
+  return hex;
 }
