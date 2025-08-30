8ae8a5ec56e3e986b0476b2df0ef3758cf28f128
Nik Nasr <nik@restate.dev>
2025-08-30 13:14:34 +0100

    update badge
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/badge/src/lib/Badge.tsx
+++ libs/ui/badge/src/lib/Badge.tsx
@@ -3,7 +3,7 @@ import { tv } from '@restate/util/styles';
 
 interface BadgeProps {
   variant?: 'success' | 'danger' | 'info' | 'warning' | 'default';
-  size?: 'sm' | 'base';
+  size?: 'sm' | 'base' | 'xs';
   className?: string;
 }
 
@@ -18,6 +18,7 @@ const styles = tv({
       default: 'border-zinc-600/10 bg-zinc-50 text-zinc-600',
     },
     size: {
+      xs: 'rounded-full px-1.5 py-[0.05rem] text-2xs leading-3 font-normal',
       sm: 'rounded-md px-1.5 py-0.5 text-xs',
       base: 'rounded-lg px-2 py-1 text-xs',
     },
