6828faf6fcbdf4172c1d72bc719867d5c60269d9
Nik Nasr <nik@restate.dev>
2025-09-15 21:02:42 +0100

    update style
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/getting-started/src/lib/GettingStarted.tsx
+++ libs/features/getting-started/src/lib/GettingStarted.tsx
@@ -18,15 +18,15 @@ export function GettingStarted({
     <div className={className}>
       <GettingStartedCard
         {...left}
-        className="absolute -bottom-1.5 left-1/2 translate-x-[-150%] translate-y-5 -rotate-12 has-hover:-bottom-0.5 has-hover:z-[2] has-hover:scale-110"
+        className="absolute -bottom-1.5 left-1/2 translate-x-[-150%] translate-y-5 -rotate-12 has-hover:-bottom-1 has-hover:z-[2] has-hover:scale-110 has-hover:pb-8 has-focus:-bottom-1 has-focus:z-[2] has-focus:scale-110 has-focus:pb-8"
       />
       <GettingStartedCard
         {...right}
-        className="absolute -bottom-1.5 left-1/2 translate-x-[50%] translate-y-5 rotate-12 has-hover:-bottom-0.5 has-hover:z-[2] has-hover:scale-110"
+        className="absolute -bottom-1.5 left-1/2 translate-x-[50%] translate-y-5 rotate-12 has-hover:-bottom-1 has-hover:z-[2] has-hover:scale-110 has-hover:pb-8 has-focus:-bottom-1 has-focus:z-[2] has-focus:scale-110 has-focus:pb-8"
       />
       <GettingStartedCard
         {...middle}
-        className="absolute -bottom-0 left-1/2 -translate-x-1/2 translate-y-0.5 rounded-b-none has-hover:bottom-1 has-hover:z-[2] has-hover:scale-110"
+        className="absolute -bottom-0 left-1/2 -translate-x-1/2 translate-y-0.5 rounded-b-none has-hover:bottom-1 has-hover:z-[2] has-hover:scale-110 has-hover:pb-6 has-focus:bottom-1 has-focus:z-[2] has-focus:scale-110 has-focus:pb-6"
       />
     </div>
   );
