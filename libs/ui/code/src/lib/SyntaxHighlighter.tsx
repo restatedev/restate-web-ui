f1307a0edfe0f3c5a4e361bcc5bec49aaa04d053
Nik Nasr <nik@restate.dev>
2025-09-29 17:51:17 +0100

    support more languages
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/code/src/lib/SyntaxHighlighter.tsx
+++ libs/ui/code/src/lib/SyntaxHighlighter.tsx
@@ -1,6 +1,8 @@
 import hljs from 'highlight.js/lib/core';
 import typescript from 'highlight.js/lib/languages/typescript';
 import java from 'highlight.js/lib/languages/java';
+import python from 'highlight.js/lib/languages/python';
+import go from 'highlight.js/lib/languages/go';
 import bash from 'highlight.js/lib/languages/bash';
 import json from 'highlight.js/lib/languages/json';
 import 'highlight.js/styles/mono-blue.css';
@@ -9,5 +11,7 @@ hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('java', java);
 hljs.registerLanguage('json', json);
+hljs.registerLanguage('go', go);
+hljs.registerLanguage('python', python);
 
 export const syntaxHighlighter = hljs;
