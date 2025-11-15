112bca7eef89754c9397b420e4fd1474731d777e
Nik Nasr <nik@restate.dev>
2025-11-15 12:16:52 +0000

    update count
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/data-access/query/src/lib/handlers/countInvocations.ts
+++ libs/data-access/query/src/lib/handlers/countInvocations.ts
@@ -3,14 +3,30 @@ import { convertInvocationsFilters } from '../convertFilters';
 import { type QueryContext } from './shared';
 
 const COUNT_LIMIT = 50000;
+const INVOCATIONS_LIMIT = 100;
 
 export async function countInvocations(
   this: QueryContext,
   filters: FilterItem[],
 ) {
-  const count = await this.query(
+  const minimumCountEstimatePromise = this.query(
     `SELECT COUNT(1) as total_count FROM (SELECT * FROM sys_invocation LIMIT ${COUNT_LIMIT}) ${convertInvocationsFilters(filters)}`,
   ).then(({ rows }) => rows?.at(0)?.total_count as number);
+  const limitCountPromise = this.query(
+    `SELECT id from sys_invocation ${convertInvocationsFilters(filters)} LIMIT ${INVOCATIONS_LIMIT}`,
+  ).then(({ rows }) => rows.length);
 
-  return Response.json({ count });
+  const [minimumCountEstimate, countWithLimit] = await Promise.all([
+    minimumCountEstimatePromise,
+    limitCountPromise,
+  ]);
+
+  if (countWithLimit < INVOCATIONS_LIMIT) {
+    return Response.json({ count: countWithLimit, isLowerBound: false });
+  } else {
+    return Response.json({
+      count: Math.max(countWithLimit, minimumCountEstimate),
+      isLowerBound: true,
+    });
+  }
 }
