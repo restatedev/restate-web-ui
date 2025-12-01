0d2859de495e22819c4c35d3e996dcb5170a062a
Nik <nik@restate.dev>
2025-12-01 13:34:31 +0000

    Update UnAuthorizedError (#459)
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/util/errors/src/lib/UnauthorizedError.ts
+++ libs/util/errors/src/lib/UnauthorizedError.ts
@@ -1,5 +1,10 @@
 export class UnauthorizedError extends Error {
   constructor() {
     super('Unauthorized Error');
+    this.name = 'UnauthorizedError';
   }
+}
+
+export function isUnauthorizedError(error: Error): error is UnauthorizedError {
+  return error.name === 'UnauthorizedError';
 }
