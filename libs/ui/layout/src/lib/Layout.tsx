c2077c2251aeb62e19929cb32876c8fd4bf69fa7
Nik Nasr <nik@restate.dev>
2025-12-06 21:02:42 +0000

    update styles
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/layout/src/lib/Layout.tsx
+++ libs/ui/layout/src/lib/Layout.tsx
@@ -11,7 +11,7 @@ export interface LayoutProps {}
 
 export function LayoutProvider({ children }: PropsWithChildren<LayoutProps>) {
   return (
-    <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col px-3 py-3 sm:px-6 sm:py-6 lg:px-8 3xl:max-w-[min(100rem,calc(100vw-800px-4rem))]">
+    <div className="mx-auto flex min-h-screen w-full max-w-[86rem] flex-col px-3 py-3 sm:px-6 sm:py-6 lg:px-8 3xl:max-w-[min(100rem,calc(100vw-800px-4rem))]">
       <AppBar id={ZONE_IDS[LayoutZone.AppBar]} />
       <NotificationRegion />
       <div className="flex flex-auto flex-row pt-10 3xl:ml-[calc(-400px-2rem)] 3xl:grid 3xl:w-[calc(100%+800px+4rem)] 3xl:[grid-template-columns:400px_1fr_400px] 3xl:gap-8">
