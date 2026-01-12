796e7e89ef123ca3bae3d655fc1d36474bea3a2e
Nik Nasr <nik@restate.dev>
2026-01-12 11:22:23 +0000

    update servicedetails merged data
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/service-details/src/lib/ServiceDetails.tsx
+++ libs/features/service-details/src/lib/ServiceDetails.tsx
@@ -133,25 +133,39 @@ function ServiceContent({ service }: { service: string }) {
   const handlerData = handlers.find(
     (handler) => handler.name === selectedHandler,
   );
+
   const resolvedData = {
     ...data,
     ...handlerData,
+    abort_timeout: handlerData?.abort_timeout ?? data?.abort_timeout,
+    inactivity_timeout:
+      handlerData?.inactivity_timeout ?? data?.inactivity_timeout,
+    journal_retention:
+      handlerData?.journal_retention ?? data?.journal_retention,
+    idempotency_retention:
+      handlerData?.idempotency_retention ?? data?.idempotency_retention,
+    enable_lazy_state:
+      handlerData?.enable_lazy_state ?? data?.enable_lazy_state,
+    public: handlerData?.public ?? data?.public,
+    workflow_completion_retention: handlerData
+      ? undefined
+      : data?.workflow_completion_retention,
     retry_policy: {
       exponentiation_factor:
-        data?.retry_policy?.exponentiation_factor ??
-        handlerData?.retry_policy?.exponentiation_factor,
+        handlerData?.retry_policy?.exponentiation_factor ??
+        data?.retry_policy?.exponentiation_factor,
       initial_interval:
-        data?.retry_policy?.initial_interval ??
-        handlerData?.retry_policy?.initial_interval,
+        handlerData?.retry_policy?.initial_interval ??
+        data?.retry_policy?.initial_interval,
       max_attempts:
-        data?.retry_policy?.max_attempts ??
-        handlerData?.retry_policy?.max_attempts,
+        handlerData?.retry_policy?.max_attempts ??
+        data?.retry_policy?.max_attempts,
       max_interval:
-        data?.retry_policy?.max_interval ??
-        handlerData?.retry_policy?.max_interval,
+        handlerData?.retry_policy?.max_interval ??
+        data?.retry_policy?.max_interval,
       on_max_attempts:
-        data?.retry_policy?.on_max_attempts ??
-        handlerData?.retry_policy?.on_max_attempts,
+        handlerData?.retry_policy?.on_max_attempts ??
+        data?.retry_policy?.on_max_attempts,
     },
   };
 
