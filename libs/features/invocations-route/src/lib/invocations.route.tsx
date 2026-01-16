708ab427978edb511d8cf5d8480025569a8fe5c7
Nik <nik@restate.dev>
2026-01-16 15:16:31 +0000

    Disable refetch on focus (#497)
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocations-route/src/lib/invocations.route.tsx
+++ libs/features/invocations-route/src/lib/invocations.route.tsx
@@ -112,6 +112,7 @@ function Component() {
     refetchOnMount: true,
     refetchOnReconnect: false,
     staleTime: 0,
+    refetchOnWindowFocus: false,
   });
 
   const dataUpdate = error ? errorUpdatedAt : dataUpdatedAt;
