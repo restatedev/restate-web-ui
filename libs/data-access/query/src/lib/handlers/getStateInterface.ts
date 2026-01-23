9ea7df38cc56935c5caf57a2b1a764c6a5be2c6b
Nik Nasr <nik@restate.dev>
2026-01-23 15:02:19 +0000

    update api
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/data-access/query/src/lib/handlers/getStateInterface.ts
+++ libs/data-access/query/src/lib/handlers/getStateInterface.ts
@@ -1,8 +1,12 @@
 import type { QueryContext } from './shared';
 
-export async function getStateInterface(this: QueryContext, service: string) {
+export async function getStateInterface(
+  this: QueryContext,
+  service: string,
+  serviceKey: string[] = [],
+) {
   const keys: { name: string }[] = await this.query(
-    `SELECT DISTINCT key FROM state WHERE service_name = '${service}' GROUP BY key`,
+    `SELECT DISTINCT key FROM state WHERE service_name = '${service}' ${serviceKey.length > 0 ? ` AND service_key IN (${serviceKey.map((key) => `'${key}'`).join(', ')})` : ''} GROUP BY key`,
   ).then(({ rows }) => rows.map((row) => ({ name: row.key })));
 
   return new Response(JSON.stringify({ keys }), {
