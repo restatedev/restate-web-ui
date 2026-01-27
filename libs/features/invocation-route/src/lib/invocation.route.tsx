a768c40a1d990b47351502a9cd7d5a12ad35fa17
Nik Nasr <nik@restate.dev>
2026-01-27 20:32:02 +0000

    fix error
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-route/src/lib/invocation.route.tsx
+++ libs/features/invocation-route/src/lib/invocation.route.tsx
@@ -44,7 +44,7 @@ const metadataContainerStyles = tv({
   },
 });
 const lastFailureContainer = tv({
-  base: 'min-w-0 origin-bottom-left rounded-xl p-0',
+  base: 'z-20 -mb-16 min-w-0 origin-bottom-left rounded-xl p-0',
 });
 const lastFailureContent = tv({
   base: 'flex-auto rounded-xl rounded-bl-none border bg-linear-to-b shadow-xl shadow-zinc-800/3 lg:mr-12 [&_.error]:max-h-72',
