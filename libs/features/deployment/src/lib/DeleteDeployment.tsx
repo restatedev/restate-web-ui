d82726049ecac5a94bb7c3d3aab66cb0896ee118
Nik Nasr <nik@restate.dev>
2026-02-12 19:28:10 +0000

    fix deployment deletion
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/deployment/src/lib/DeleteDeployment.tsx
+++ libs/features/deployment/src/lib/DeleteDeployment.tsx
@@ -12,7 +12,7 @@ import { Form, useSearchParams } from 'react-router';
 import { Button, SubmitButton } from '@restate/ui/button';
 import { ErrorBanner } from '@restate/ui/error';
 import { FormFieldInput } from '@restate/ui/form-field';
-import { FormEvent, useId, useState } from 'react';
+import { FormEvent, PropsWithChildren, useId, useState } from 'react';
 import { getEndpoint } from '@restate/data-access/admin-api';
 import {
   useDeleteDeployment,
@@ -21,6 +21,32 @@ import {
 import { showSuccessNotification } from '@restate/ui/notification';
 import { useRestateContext } from '@restate/features/restate-context';
 import { ONBOARDING_QUERY_PARAM } from '@restate/util/feature-flag';
+import { TruncateWithTooltip } from '@restate/ui/tooltip';
+import { Icon, IconName } from '@restate/ui/icons';
+import { tv } from '@restate/util/styles';
+import { Link } from '@restate/ui/link';
+
+const warningStyles = tv({
+  base: 'flex gap-2 rounded-xl border border-orange-200 bg-orange-50 px-3 py-2 text-0.5xs text-orange-600',
+});
+export function Warning({
+  title,
+  className,
+  children,
+}: PropsWithChildren<{ title?: string; className?: string }>) {
+  return (
+    <p className={warningStyles({ className })}>
+      <Icon
+        className="h-5 w-5 shrink-0 fill-orange-600 text-orange-100"
+        name={IconName.TriangleAlert}
+      />
+      <span className="inline-block">
+        {title && <span className="font-semibold">{title}: </span>}
+        {children}
+      </span>
+    </p>
+  );
+}
 
 export function DeleteDeployment() {
   const formId = useId();
@@ -79,18 +105,28 @@ export function DeleteDeployment() {
 
   return (
     <QueryDialog query={DELETE_DEPLOYMENT_QUERY_PARAM}>
-      <DialogContent>
+      <DialogContent className="max-w-lg">
         <div className="flex flex-col gap-2">
           <h3 className="text-lg leading-6 font-medium text-gray-900">
             Confirm Deployment deletion
           </h3>
           <p className="text-sm text-gray-500">
-            Are you sure you want to delete{' '}
-            <code className="inline-block rounded-md bg-red-50 p-0.5 text-red-700 ring-red-600/10">
-              {deploymentEndpoint}
+            Are you sure you want to delete this deployment?
+            <code className="my-1 block max-w-fit rounded-md bg-red-50 p-0.5 text-red-700 ring-red-600/10">
+              <TruncateWithTooltip>{deploymentEndpoint}</TruncateWithTooltip>
             </code>
-            ? This might break in-flight invocations, use with{' '}
-            <span className="font-medium">caution</span>.
+            <Warning className="mt-4">
+              This might break in-flight invocations, use with{' '}
+              <span className="font-medium">caution</span>.{' '}
+              <Link
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://docs.restate.dev/services/versioning#removing-a-service"
+                className="text-orange-700 decoration-orange-700"
+              >
+                Learn more…
+              </Link>
+            </Warning>
           </p>
           <Form
             id={formId}
