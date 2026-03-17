21f7b744fa67baca6012907b74bc92297dda3f17
Nik Nasr <nik@restate.dev>
2026-03-17 13:39:55 +0000

    remove duration
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocations-summary/src/index.ts
+++ libs/features/invocations-summary/src/index.ts
@@ -1,6 +1,4 @@
 export { InvocationsSummary } from './lib/invocations-summary';
-export { DurationPercentiles } from './lib/duration-percentiles';
-export type { DurationPercentilesData } from './lib/duration-percentiles';
 export { MOCK_SCENARIOS } from './lib/mock-scenarios';
 export { STATUS_COLUMNS } from './lib/constants';
 export type {
