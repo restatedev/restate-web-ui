1d869ef3996501db45877fd00da3e9cabcb3bdb8
Nik Nasr <nik@restate.dev>
2026-05-05 16:35:23 +0100

    add tabs
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ libs/ui/tabs/README.md
@@ -0,0 +1,3 @@
+# tabs
+
+This library was generated with [Nx](https://nx.dev).
---
+++ libs/ui/tabs/eslint.config.js
@@ -0,0 +1,25 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const baseConfig = require('../../../eslint.config.js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  ...baseConfig,
+  ...compat.extends('plugin:@nx/react'),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+];
