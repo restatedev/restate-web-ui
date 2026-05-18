dc3b3f4febc3c7747b51894c234e7211ee71b0c2
Nik Nasr <nik@restate.dev>
2026-05-18 19:45:46 +0100

    update style
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/overview2-route/src/lib/cardShell.ts
+++ libs/features/overview2-route/src/lib/cardShell.ts
@@ -11,8 +11,8 @@ export const cardInnerStyles = tv({
   variants: {
     issueSeverity: {
       none: '',
-      low: 'before:pointer-events-none before:absolute before:inset-y-0 before:right-0 before:-left-6 before:z-10 before:animate-stripeScroll before:bg-[repeating-linear-gradient(135deg,transparent,transparent_8px,--theme(--color-orange-500/8%)_8px,--theme(--color-orange-500/8%)_16px)] before:[mask-image:linear-gradient(to_top_left,transparent_calc(100%-3rem),black_100%)] after:pointer-events-none after:absolute after:inset-0 after:z-10 after:bg-[linear-gradient(to_top_left,transparent_40%,--theme(--color-orange-500/12%))] after:[mask-image:linear-gradient(to_top_left,transparent_calc(100%-3rem),black_100%)]',
-      high: 'before:pointer-events-none before:absolute before:inset-y-0 before:right-0 before:-left-6 before:z-10 before:animate-stripeScroll before:bg-[repeating-linear-gradient(135deg,transparent,transparent_8px,--theme(--color-red-500/8%)_8px,--theme(--color-red-500/8%)_16px)] before:[mask-image:linear-gradient(to_top_left,transparent_calc(100%-3rem),black_100%)] after:pointer-events-none after:absolute after:inset-0 after:z-10 after:bg-[linear-gradient(to_top_left,transparent_40%,--theme(--color-red-500/12%))] after:[mask-image:linear-gradient(to_top_left,transparent_calc(100%-3rem),black_100%)]',
+      low: 'bg-[linear-gradient(to_right,--theme(--color-orange-50)_0,white_4rem)]',
+      high: 'bg-[linear-gradient(to_right,--theme(--color-red-50)_0,white_4rem)]',
     },
   },
   defaultVariants: {
