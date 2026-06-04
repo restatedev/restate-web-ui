82b3b9dc4b2df03550aca9932cc50a0a5209ebb5
Nik Nasr <nik@restate.dev>
2026-06-04 14:52:21 +0100

    fix lint
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/util/remix/src/lib/useHydrated.ts
+++ libs/util/remix/src/lib/useHydrated.ts
@@ -1,5 +1,6 @@
 import { useSyncExternalStore } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 const subscribe = () => () => {};
 const getSnapshot = () => true;
 const getServerSnapshot = () => false;
