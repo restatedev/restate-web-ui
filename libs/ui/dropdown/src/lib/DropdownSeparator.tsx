9f32ed01a471f93ac778fea596dc8358890b3fe1
Nik Nasr <nik@restate.dev>
2026-06-10 20:20:06 +0100

    update dialog
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/ui/dropdown/src/lib/DropdownSeparator.tsx
+++ libs/ui/dropdown/src/lib/DropdownSeparator.tsx
@@ -2,7 +2,7 @@ import { tv } from '@restate/util/styles';
 import { Separator } from 'react-aria-components';
 
 const styles = tv({
-  base: 'mx-0 my-0 border-t-0 border-b',
+  base: '-mx-1 my-0 border-t-0 border-b',
 });
 export function DropdownSeparator({ className }: { className?: string }) {
   return <Separator className={styles({ className })} />;
