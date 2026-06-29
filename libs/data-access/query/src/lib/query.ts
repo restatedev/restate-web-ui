f3aea3f076be3d358110239062c97d12defd33e2
Nik Nasr <nik@restate.dev>
2026-06-29 13:44:06 +0100

    update query
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/data-access/query/src/lib/query.ts
+++ libs/data-access/query/src/lib/query.ts
@@ -53,6 +53,8 @@ type BoundHandlers = {
   listInvocations: (
     filters: FilterItem[],
     sort?: components['schemas']['ListInvocationsRequestBody']['sort'],
+    sampled?: boolean,
+    sampleSize?: number,
   ) => Promise<Response>;
   countInvocations: (filters: FilterItem[]) => Promise<Response>;
   summaryInvocations: (
@@ -334,9 +336,11 @@ router.map(routes, {
         const {
           filters = [],
           sort,
+          sampled,
+          sampleSize,
         }: components['schemas']['ListInvocationsRequestBody'] =
           await ctx.request.json();
-        return listInvocations(filters, sort);
+        return listInvocations(filters, sort, sampled, sampleSize);
       },
       async count(ctx) {
         const { countInvocations } = ctx.storage.get(handlersKey);
