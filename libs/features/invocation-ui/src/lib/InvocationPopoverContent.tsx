63f89129ed210f7917cbd2eb096f5b646dcdd14f
Nik Nasr <nik@restate.dev>
2026-07-06 13:55:24 +0100

    update popover
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- libs/features/invocation-ui/src/lib/InvocationPopoverContent.tsx
+++ libs/features/invocation-ui/src/lib/InvocationPopoverContent.tsx
@@ -1,27 +1,40 @@
 import { useGetInvocation } from '@restate/data-access/admin-api-hooks';
-import { Target } from './Target';
 import { Copy } from '@restate/ui/copy';
+import { DropdownSection } from '@restate/ui/dropdown';
 import { Status } from './Status';
+import { Target } from './Target';
 
 export function InvocationPopoverContent({ id }: { id: string }) {
   const { data: invocation } = useGetInvocation(id, {
     refetchOnMount: 'always',
   });
   return (
-    <div className="flex max-w-2xl flex-col items-start gap-1.5 px-3 py-2.5">
-      {invocation && (
-        <Target target={invocation.target} className="max-w-full flex-none" />
-      )}
-      <div className="flex max-w-full flex-wrap items-center gap-x-3 gap-y-1.5">
-        <div className="flex min-w-0 items-center gap-1.5">
-          <span className="truncate font-mono text-xs text-zinc-600">{id}</span>
-          <Copy
-            copyText={id}
-            className="h-5 w-5 shrink-0 rounded-md border bg-white p-1 text-gray-700 shadow-xs"
-          />
+    <div className="max-w-2xl min-w-72">
+      <DropdownSection
+        title={
+          <span className="flex min-w-0 items-center gap-1.5">
+            <span className="truncate font-mono text-xs font-medium text-zinc-600">
+              {id}
+            </span>
+            <Copy
+              copyText={id}
+              className="s ml-0 h-5 w-5 shrink-0 rounded-md p-1 text-gray-700"
+            />
+          </span>
+        }
+      >
+        <div className="flex flex-wrap items-start gap-3 px-3 py-2.5">
+          {invocation && (
+            <>
+              <Target
+                target={invocation.target}
+                className="max-w-full flex-none"
+              />
+              <Status invocation={invocation} />
+            </>
+          )}
         </div>
-        {invocation && <Status invocation={invocation} />}
-      </div>
+      </DropdownSection>
     </div>
   );
 }
